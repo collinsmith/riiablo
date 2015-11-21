71b08203420cbdb05a9359574d3bc6545a18c161
Collin Smith <collinsmith70@gmail.com>
2015-11-20 22:58:17 -0800

    Added additional Command for getting and checking binds Added additional Command for seeing all command binds Added ParameterResolvers for key aliases (command binds) and keys (i.e., string representations for keys on the keyboard)

--- core/src/com/google/collinsmith70/diablo/command/Commands.java
+++ core/src/com/google/collinsmith70/diablo/command/Commands.java
@@ -1,9 +1,13 @@
 package com.google.collinsmith70.diablo.command;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.google.collinsmith70.diablo.Client;
 import com.google.collinsmith70.diablo.command.resolver.CvarParameterResolver;
+import com.google.collinsmith70.diablo.command.resolver.KeyAliasParameterResolver;
+import com.google.collinsmith70.diablo.command.resolver.KeyParameterResolver;
 import com.google.collinsmith70.diablo.cvar.Cvar;
+import com.google.collinsmith70.diablo.key.Key;
 
 import java.lang.reflect.Field;
 
@@ -87,4 +91,64 @@ public static final Command RESET = new Command("reset",
             }
         },
         CvarParameterResolver.INSTANCE);
+
+public static final Command BIND = new Command("bind",
+        new Action() {
+            @Override
+            public void onActionExecuted(Client client, String[] args) {
+                Key key;
+                switch (args.length) {
+                    case 2:
+                        key = Key.get(args[1]);
+                        if (key == null) {
+                            client.getConsole().log(String.format(
+                                    "Unrecognized key alias \"%s\". To see available key aliases, type \"%s\"",
+                                    args[1],
+                                    Commands.BINDS.getCommand()));
+                            break;
+                        }
+
+                        client.getConsole().log("bind " + key.getAlias() + " = " + key.getKeysString());
+                        break;
+                    case 3:
+                        key = Key.get(args[1]);
+                        if (key == null) {
+                            client.getConsole().log(String.format(
+                                    "Unrecognized key alias \"%s\". To see available key aliases, type \"%s\"",
+                                    args[1],
+                                    Commands.BINDS.getCommand()));
+                            break;
+                        }
+
+                        int keycode = Input.Keys.valueOf(args[2]);
+                        if (keycode == -1) {
+                            client.getConsole().log(String.format(
+                                    "Unrecognized key value \"%s\".",
+                                    args[2]));
+                            break;
+                        }
+
+                        key.addKey(keycode);
+                        client.getConsole().log("bind " + key.getAlias() + " = " + key.getKeysString());
+                        break;
+                    default:
+                        client.getConsole().log(String.format(
+                                "Invalid number of parameters: %d. Expected 2 or 3",
+                                args.length));
+                }
+            }
+        },
+        KeyAliasParameterResolver.INSTANCE,
+        new OptionalParameterResolver<KeyParameterResolver>(KeyParameterResolver.INSTANCE));
+
+public static final Command BINDS = new Command("binds",
+        new Action() {
+            @Override
+            public void onActionExecuted(Client client, String[] args) {
+                for (Key key : Key.getAllKeys()) {
+                    client.getConsole().log(key.toString());
+                }
+            }
+        });
+
 }
