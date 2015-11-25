79494feccd36aa8debc7da7c206b76d967dbb725
Collin Smith <collinsmith70@gmail.com>
2015-11-24 20:16:56 -0800

    Added Keys for Belt and re-ordered SKILL_* keys a bit.

--- core/src/com/google/collinsmith70/diablo/key/Keys.java
+++ core/src/com/google/collinsmith70/diablo/key/Keys.java
@@ -27,15 +27,20 @@ public static void init(Class<?> clazz) {
 
 public static final Key CONSOLE = new Key("console", Input.Keys.GRAVE);
 
-public static final Key SKILL_0 = new Key("skill_0", Input.Keys.Q);
-public static final Key SKILL_1 = new Key("skill_1", Input.Keys.W);
-public static final Key SKILL_2 = new Key("skill_2", Input.Keys.E);
-public static final Key SKILL_3 = new Key("skill_3", Input.Keys.R);
-public static final Key SKILL_4 = new Key("skill_4", Input.Keys.T);
-public static final Key SKILL_5 = new Key("skill_5", Input.Keys.A);
-public static final Key SKILL_6 = new Key("skill_6", Input.Keys.S);
-public static final Key SKILL_7 = new Key("skill_7", Input.Keys.D);
-public static final Key SKILL_8 = new Key("skill_8", Input.Keys.F);
-public static final Key SKILL_9 = new Key("skill_9", Input.Keys.G);
+public static final Key SKILL_1 = new Key("skill_1", Input.Keys.Q);
+public static final Key SKILL_2 = new Key("skill_2", Input.Keys.W);
+public static final Key SKILL_3 = new Key("skill_3", Input.Keys.E);
+public static final Key SKILL_4 = new Key("skill_4", Input.Keys.R);
+public static final Key SKILL_5 = new Key("skill_5", Input.Keys.T);
+public static final Key SKILL_6 = new Key("skill_6", Input.Keys.A);
+public static final Key SKILL_7 = new Key("skill_7", Input.Keys.S);
+public static final Key SKILL_8 = new Key("skill_8", Input.Keys.D);
+public static final Key SKILL_9 = new Key("skill_9", Input.Keys.F);
+public static final Key SKILL_0 = new Key("skill_0", Input.Keys.G);
+
+public static final Key BELT_1 = new Key("belt_1", Input.Keys.NUM_1);
+public static final Key BELT_2 = new Key("belt_2", Input.Keys.NUM_2);
+public static final Key BELT_3 = new Key("belt_3", Input.Keys.NUM_3);
+public static final Key BELT_4 = new Key("belt_4", Input.Keys.NUM_4);
 
 }
