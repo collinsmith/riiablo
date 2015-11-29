7c2db82ca4dce5734efea94ea9dd6c1dc3a93493
Collin Smith <collinsmith70@gmail.com>
2015-11-29 12:17:17 -0800

    Abstracted out GUI portions from Console into ConsoleView Abstracted Caret class to manage timers and Caret-specific configurations

--- core/src/com/google/collinsmith70/diablo/Console.java
+++ core/src/com/google/collinsmith70/diablo/Console.java
@@ -1,66 +1,217 @@
 package com.google.collinsmith70.diablo;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.utils.StringBuilder;
+import com.google.collinsmith70.diablo.cvar.Cvar;
+import com.google.collinsmith70.diablo.cvar.CvarChangeListener;
+import com.google.collinsmith70.diablo.cvar.Cvars;
 
+import java.io.PrintStream;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-public class Console {
+public class Console implements InputProcessor {
 
+private static final String TAG = Console.class.getSimpleName();
 private static final int CONSOLE_BUFFER_SIZE = 256;
 
 private final Client CLIENT;
-private final Set<CommandProcessor> commandProcessors;
+private final PrintStream OUTPUT_STREAM;
+private final Set<CommandProcessor> COMMAND_PROCESSORS;
 
 private StringBuilder buffer;
-private boolean isVisible;
+private int position;
+
+private String bufferPrefix;
 
 public Console(Client client) {
+    this(client, System.out);
+}
+
+public Console(Client client, PrintStream outputStream) {
     this.CLIENT = client;
-    this.commandProcessors = new CopyOnWriteArraySet<CommandProcessor>();
-    this.isVisible = false;
+    this.OUTPUT_STREAM = outputStream;
+    this.COMMAND_PROCESSORS = new CopyOnWriteArraySet<CommandProcessor>();
+
+    Cvars.Client.Overlay.CommandPrefix.addCvarChangeListener(new CvarChangeListener<String>() {
+        @Override
+        public void onCvarChanged(Cvar<String> cvar, String fromValue, String toValue) {
+            Console.this.bufferPrefix = toValue;
+        }
+    });
+
+    clearBuffer();
 }
 
 public Client getClient() {
     return CLIENT;
 }
 
-public boolean isVisible() {
-    return isVisible;
+public PrintStream getOutputStream() {
+    return OUTPUT_STREAM;
 }
 
-public void setVisible(boolean b) {
-    this.isVisible = b;
-    Gdx.input.setOnscreenKeyboardVisible(isVisible());
-    if (isVisible()) {
-        //...
-    }
+public String getBufferPrefix() {
+    return bufferPrefix;
+}
+
+public void setBufferPrefix(String bufferPrefix) {
+    this.bufferPrefix = bufferPrefix;
 }
 
 public void addCommandProcessor(CommandProcessor p) {
-    commandProcessors.add(p);
+    COMMAND_PROCESSORS.add(p);
 }
 
 public boolean containsCommandProcessor(CommandProcessor p) {
-    return commandProcessors.contains(p);
+    return COMMAND_PROCESSORS.contains(p);
 }
 
 public boolean removeCommandProcessor(CommandProcessor p) {
-    return commandProcessors.remove(p);
+    return COMMAND_PROCESSORS.remove(p);
 }
 
 public void clearBuffer() {
     buffer = new StringBuilder(CONSOLE_BUFFER_SIZE);
-    //caretPosition = 0;
+    setPosition(0);
 }
 
 public String getBuffer() {
     return buffer.toString();
 }
 
-public String[] split() {
-    return buffer.toString().split(";");
+public int getPosition() {
+    return position;
+}
+
+public void setPosition(int position) {
+    if (this.position == position) {
+        return;
+    } else if (position < 0) {
+        position = 0;
+    } else if (position > buffer.length()) {
+        position = buffer.length();
+    }
+
+    this.position = position;
+}
+
+public static String[] splitBuffer(String buffer) {
+    return buffer.split(";");
+}
+
+public String[] splitBuffer() {
+    return Console.splitBuffer(buffer.toString());
+}
+
+@Override
+public boolean keyDown(int keycode) {
+    switch (keycode) {
+        // TODO: Keys.Console should trigger this as well
+        case Input.Keys.LEFT:
+            setPosition(getPosition() - 1);
+            return true;
+        case Input.Keys.RIGHT:
+            setPosition(getPosition() + 1);
+            return true;
+        case Input.Keys.UP:
+            return true;
+        case Input.Keys.DOWN:
+            return true;
+        case Input.Keys.TAB:
+            return true;
+        default:
+            return false;
+    }
+}
+
+@Override
+public boolean keyUp(int keycode) {
+    return false;
+}
+
+@Override
+public boolean keyTyped(char ch) {
+    int position = getPosition();
+    switch (ch) {
+        case '\b':
+            if (position > 0) {
+                buffer.deleteCharAt(position - 1);
+                setPosition(position - 1);
+            }
+
+            return true;
+        case '\r':
+        case '\n':
+            String bufferContents = buffer.toString();
+            if (bufferContents.isEmpty()) {
+                return true;
+            }
+
+            log(bufferPrefix + " " + bufferContents);
+
+            boolean handled;
+            for (String command : Console.splitBuffer(bufferContents)) {
+                handled = false;
+                Gdx.app.log(TAG, bufferPrefix + " " + command);
+                for (CommandProcessor p : COMMAND_PROCESSORS) {
+                    if (p.process(command)) {
+                        handled = true;
+                        break;
+                    }
+                }
+
+                if (!handled) {
+                    Gdx.app.log(TAG, String.format("Unrecognized command: \"%s\"", command));
+                }
+            }
+
+            clearBuffer();
+            return true;
+        case 127: // DEL
+            if (position < buffer.length()) {
+                buffer.deleteCharAt(position);
+            }
+
+            setPosition(position);
+            return true;
+        default:
+            buffer.insert(position, ch);
+            setPosition(position + 1);
+            return true;
+    }
+}
+
+@Override
+public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+    return true;
+}
+
+@Override
+public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+    return true;
+}
+
+@Override
+public boolean touchDragged(int screenX, int screenY, int pointer) {
+    return true;
+}
+
+@Override
+public boolean mouseMoved(int screenX, int screenY) {
+    return true;
+}
+
+@Override
+public boolean scrolled(int amount) {
+    return true;
+}
+
+public void log(String message) {
+    Gdx.app.log(TAG, message);
+    OUTPUT_STREAM.println(message);
 }
 
 }
---
+++ core/src/com/google/collinsmith70/diablo/ConsoleView.java
@@ -0,0 +1,217 @@
+package com.google.collinsmith70.diablo;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.Timer;
+import com.google.collinsmith70.diablo.cvar.Cvar;
+import com.google.collinsmith70.diablo.cvar.CvarChangeListener;
+import com.google.collinsmith70.diablo.cvar.Cvars;
+
+public class ConsoleView extends Console implements Disposable {
+
+private final Caret CARET;
+private final Texture modelBackgroundTexture;
+
+private boolean isVisible;
+private BitmapFont font;
+private float outputOffset;
+
+public ConsoleView(Client client) {
+    super(client);
+    this.isVisible = false;
+
+    this.CARET = new Caret();
+
+    Pixmap solidColorPixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+    solidColorPixmap.setColor(0.0f, 0.0f, 0.0f, 0.5f);
+    solidColorPixmap.fill();
+    modelBackgroundTexture = new Texture(solidColorPixmap);
+    solidColorPixmap.dispose();
+    solidColorPixmap = null;
+
+    final CvarChangeListener<Float> consoleFontColorCvarListener = new CvarChangeListener<Float>() {
+        @Override
+        public void onCvarChanged(Cvar<Float> cvar, Float fromValue, Float toValue) {
+            if (cvar.equals(Cvars.Client.Overlay.ConsoleFontColor.a)) {
+                font.getColor().a = toValue;
+            } else if (cvar.equals(Cvars.Client.Overlay.ConsoleFontColor.r)) {
+                font.getColor().r = toValue;
+            } else if (cvar.equals(Cvars.Client.Overlay.ConsoleFontColor.g)) {
+                font.getColor().g = toValue;
+            } else if (cvar.equals(Cvars.Client.Overlay.ConsoleFontColor.b)) {
+                font.getColor().b = toValue;
+            }
+        }
+    };
+
+    Cvars.Client.Overlay.ConsoleFont.addCvarChangeListener(new CvarChangeListener<AssetDescriptor<BitmapFont>>() {
+        @Override
+        public void onCvarChanged(Cvar<AssetDescriptor<BitmapFont>> cvar, AssetDescriptor<BitmapFont> fromValue, AssetDescriptor<BitmapFont> toValue) {
+            AssetManager assetManager = getClient().getAssetManager();
+            assetManager.load(toValue);
+            assetManager.finishLoading();
+
+            ConsoleView.this.font = assetManager.get(Cvars.Client.Overlay.ConsoleFont.getValue());
+            Cvars.Client.Overlay.ConsoleFontColor.r.addCvarChangeListener(consoleFontColorCvarListener);
+            Cvars.Client.Overlay.ConsoleFontColor.g.addCvarChangeListener(consoleFontColorCvarListener);
+            Cvars.Client.Overlay.ConsoleFontColor.b.addCvarChangeListener(consoleFontColorCvarListener);
+            Cvars.Client.Overlay.ConsoleFontColor.a.addCvarChangeListener(consoleFontColorCvarListener);
+        }
+    });
+}
+
+public boolean isVisible() {
+    return isVisible;
+}
+
+public void setVisible(boolean b) {
+    this.isVisible = b;
+    Gdx.input.setOnscreenKeyboardVisible(isVisible());
+    if (isVisible()) {
+        //...
+    }
+}
+
+public BitmapFont getFont() {
+    return font;
+}
+
+public void clear() {
+    // TODO: add abstraction to clear the console (which is separate from stream)
+}
+
+@Override
+public void setPosition(int position) {
+    super.setPosition(position);
+    if (CARET != null) {
+        CARET.setVisible(true);
+    }
+}
+
+public void render(Batch b) {
+    if (!isVisible()) {
+        return;
+    }
+
+    b.draw(modelBackgroundTexture, 0.0f, 0.0f, getClient().getVirtualWidth(), getClient().getVirtualHeight());
+    GlyphLayout glyphs = font.draw(b, getBufferPrefix() + " " + getBuffer(), 0, getClient().getVirtualHeight());
+
+    glyphs.setText(font, getBufferPrefix() + " " + getBuffer().substring(0, getPosition()));
+    CARET.render(b, font, glyphs, getClient().getVirtualHeight() - 1);
+}
+
+@Override
+public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+    Gdx.input.setOnscreenKeyboardVisible(true);
+    return super.touchDown(screenX, screenY, pointer, button);
+}
+
+public boolean scrolled(int amount) {
+    switch (amount) {
+        case -1:
+            //outputOffset = Math.min(
+            //        outputOffset + font.getLineHeight(),
+            //        getClient().getVirtualHeight() - 2*font.getLineHeight());
+            break;
+        case 1:
+            //outputOffset = Math.max(
+            //        outputOffset - font.getLineHeight(),
+            //        getClient().getVirtualHeight() - (getOutputStream() * font.getLineHeight()));
+            break;
+    }
+
+    return true;
+}
+
+@Override
+public void dispose() {
+    font.dispose();
+}
+
+private static class Caret {
+
+    static final float DEFAULT_HOLD_DELAY = 1.0f;
+    static final float DEFAULT_BLINK_DELAY = 0.5f;
+
+    final Timer TIMER;
+    final Timer.Task BLINK_TASK;
+
+    float holdDelay;
+    float blinkDelay;
+
+    boolean isVisible;
+
+    Caret() {
+        this(DEFAULT_HOLD_DELAY, DEFAULT_BLINK_DELAY);
+    }
+
+    Caret(float holdDelay, float blinkDelay) {
+        if (holdDelay < 0.1f) {
+            throw new IllegalArgumentException("holdDelay should be >= 0.1f");
+        }
+
+        if (blinkDelay < 0.1f) {
+            throw new IllegalArgumentException("blinkDelay should be >= 0.1f");
+        }
+
+        this.holdDelay = holdDelay;
+        this.blinkDelay = blinkDelay;
+
+        this.isVisible = true;
+
+        TIMER = new Timer();
+        BLINK_TASK = new Timer.Task() {
+            @Override
+            public void run() {
+                Caret.this.setVisible(!Caret.this.isVisible());
+            }
+        };
+
+        TIMER.start();
+        setVisible(true);
+    }
+
+    boolean isVisible() {
+        return isVisible;
+    }
+
+    void setVisible(boolean b) {
+        this.isVisible = b;
+        BLINK_TASK.cancel();
+        TIMER.schedule(BLINK_TASK, getHoldDelay(), getBlinkDelay());
+    }
+
+    float getHoldDelay() {
+        return holdDelay;
+    }
+
+    void setHoldDelay(float holdDelay) {
+        this.holdDelay = holdDelay;
+    }
+
+    float getBlinkDelay() {
+        return blinkDelay;
+    }
+
+    void setBlinkDelay(float blinkDelay) {
+        this.blinkDelay = blinkDelay;
+    }
+
+    void render(Batch batch, BitmapFont font, GlyphLayout glyphs, float height) {
+        if (!isVisible()) {
+            return;
+        }
+
+        font.draw(batch, "_", glyphs.width - 4, height);
+    }
+
+}
+
+}
