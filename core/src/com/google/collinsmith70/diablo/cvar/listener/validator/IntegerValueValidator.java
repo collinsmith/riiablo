0d71b0555526bc2a386bd7eb8322a33e5569646f
Collin Smith <collinsmith70@gmail.com>
2015-12-28 23:12:28 -0800

    Added IntegerValueValidator

---
+++ core/src/com/google/collinsmith70/diablo/cvar/listener/validator/IntegerValueValidator.java
@@ -0,0 +1,45 @@
+package com.google.collinsmith70.diablo.cvar.listener.validator;
+
+import com.badlogic.gdx.Gdx;
+import com.google.collinsmith70.diablo.cvar.Cvar;
+import com.google.collinsmith70.diablo.cvar.CvarValueValidator;
+
+public class IntegerValueValidator implements CvarValueValidator<Integer> {
+
+private static final String TAG = FloatValueValidator.class.getSimpleName();
+
+private final int min;
+private final int max;
+
+public IntegerValueValidator(int min, int max) {
+    this.min = min;
+    this.max = max;
+}
+
+public IntegerValueValidator(int min) {
+    this.min = min;
+    this.max = Integer.MAX_VALUE;
+}
+
+@Override
+public Integer onValidateValue(Cvar<Integer> cvar, Integer fromValue, Integer toValue) {
+    if (toValue < min) {
+        Gdx.app.log(TAG, String.format(
+                "%s casted from %d to %d",
+                cvar.getKey(),
+                toValue,
+                min));
+        return min;
+    } else if (toValue > max) {
+        Gdx.app.log(TAG, String.format(
+                "%s casted from %d to %d",
+                cvar.getKey(),
+                toValue,
+                max));
+        return max;
+    }
+
+    return toValue;
+}
+
+}
