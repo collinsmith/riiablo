90c353d1bfa2cc4025f2d27cff2d0805cd485523
Collin Smith <collinsmith70@gmail.com>
2016-01-06 21:15:35 -0800

    Added LinkedList implementation of FixedSizeArrayList which performs the remove operations more efficiently if the first element is removed once the list has reached capacity

---
+++ core/src/com/gmail/collinsmith70/util/FixedSizeLinkedList.java
@@ -0,0 +1,28 @@
+package com.gmail.collinsmith70.util;
+
+import java.util.LinkedList;
+
+public class FixedSizeLinkedList<E> extends LinkedList<E> {
+
+private static final int DEFAULT_CAPACITY = 1<<8;
+
+private int capacity;
+
+public FixedSizeLinkedList() {
+    this(DEFAULT_CAPACITY);
+}
+
+public FixedSizeLinkedList(int capacity) {
+    this.capacity = capacity;
+}
+
+@Override
+public boolean add(E e) {
+    if (super.size() == capacity) {
+        super.remove(0);
+    }
+
+    return super.add(e);
+}
+
+}
