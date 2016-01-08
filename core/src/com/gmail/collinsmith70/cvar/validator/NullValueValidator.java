e6f4cc8b35712ed952bbfff82da7366216e29951
Collin Smith <collinsmith70@gmail.com>
2016-01-07 19:44:06 -0800

    Added documentation for NullValueValidator

--- core/src/com/gmail/collinsmith70/cvar/validator/NullValueValidator.java
+++ core/src/com/gmail/collinsmith70/cvar/validator/NullValueValidator.java
@@ -2,7 +2,14 @@ package com.gmail.collinsmith70.cvar.validator;
 
 import com.gmail.collinsmith70.cvar.ValueValidator;
 
+/**
+ * A {@linkplain NullValueValidator} is a {@link ValueValidator} which validates all values as
+ * {@literal true}.
+ */
 public enum NullValueValidator implements ValueValidator {
+/**
+ * @see NullValueValidator
+ */
 INSTANCE;
 
 @Override
