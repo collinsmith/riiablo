254be0177100bb4a357477664e22f5b6f6510866
Collin Smith <collinsmith70@gmail.com>
2016-02-02 01:32:27 -0800

    Added additional fields to store the arbitrary min and max of the range

--- core/src/com/gmail/collinsmith70/util/RangeValidatorException.java
+++ core/src/com/gmail/collinsmith70/util/RangeValidatorException.java
@@ -2,12 +2,27 @@ package com.gmail.collinsmith70.util;
 
 public class RangeValidatorException extends ValidatorException {
 
+private final Object MIN;
+private final Object MAX;
+
 public RangeValidatorException(String reason) {
     super(reason);
+    this.MIN = null;
+    this.MAX = null;
 }
 
 public RangeValidatorException(Object min, Object max) {
     super(String.format("passed reference must lie between %s and %s (inclusive)", min, max));
+    this.MIN = min;
+    this.MAX = max;
+}
+
+public Object getMin() {
+    return MIN;
+}
+
+public Object getMax() {
+    return MAX;
 }
 
 }
