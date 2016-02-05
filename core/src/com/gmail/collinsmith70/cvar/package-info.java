a218ada91ffa28e7ab6ed7a024adaef7183e0199
Collin Smith <collinsmith70@gmail.com>
2016-02-04 19:51:37 -0800

    Added package-info.java for com.gmail.collinsmith70.cvar

---
+++ core/src/com/gmail/collinsmith70/cvar/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * This package contains classes useful in {@linkplain com.gmail.collinsmith70.cvar.CvarManager
+ * creating and managing} {@link com.gmail.collinsmith70.cvar.Cvar} instances.
+ * <p>
+ * <a href="https://en.wikipedia.org/wiki/CVAR">CVAR</a>s are representations for variables which
+ * are used for configuring some part of a client, specifically in game applications.
+ * {@link com.gmail.collinsmith70.cvar.Cvar} instances are commonly
+ * {@linkplain com.gmail.collinsmith70.cvar.SaveableCvarManager#save saved} so that their state
+ * can persist across multiple runs of the client, e.g., settings.
+ * </p>
+ *
+ * @author csmith
+ * @version 1.0
+ */
+package com.gmail.collinsmith70.cvar;
+
