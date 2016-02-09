a2d6b0b4f8e3952d26cc1802a0ed4dba9055b275
Collin Smith <collinsmith70@gmail.com>
2016-02-09 13:35:06 -0800

    Added centerAt with documentation reflecting how the method reacts to a null argument

--- core/src/com/gmail/collinsmith70/unifi/widget/Dialog.java
+++ core/src/com/gmail/collinsmith70/unifi/widget/Dialog.java
@@ -1,5 +1,13 @@
 package com.gmail.collinsmith70.unifi.widget;
 
-public class Dialog {
+import android.support.annotation.Nullable;
+
+public interface Dialog {
+
+/**
+ * @param widget {@link Widget} to center this {@link Dialog} relative to, or {@code null} to center
+ *               within the {@link Window} container of this {@link Dialog}.
+ */
+void centerAt(@Nullable final Widget widget);
 
 }
