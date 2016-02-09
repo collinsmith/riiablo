301b6ab3d993e61a7e15c2d15d237404b8d3dcac
Collin Smith <collinsmith70@gmail.com>
2016-02-09 14:45:48 -0800

    Added setters for layout height and width. I think it might be best to incorporate bitflag fields in this case to store the state of not just this field but other boolean fields

--- core/src/com/gmail/collinsmith70/unifi/widget/Widget.java
+++ core/src/com/gmail/collinsmith70/unifi/widget/Widget.java
@@ -1,8 +1,21 @@
 package com.gmail.collinsmith70.unifi.widget;
 
+import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 
 public abstract class Widget {
+
+/**
+ * {@linkplain Enum Enumeration} of all {@link Visibility} modifiers a {@link Widget} may have.
+ */
+public enum Visibility {
+    /** {@link Widget} will be laid out and rendered normally */
+    VISIBLE,
+    /** {@link Widget} will be laid out normally, however it will not be drawn */
+    INVISIBLE,
+    /** {@link Widget} will neither be laid out or drawn (as if it is not even present) */
+    GONE
+}
 
 /**
  * {@link Widget} will be sized large enough to fit all of its components
@@ -78,15 +91,41 @@ public void setVisibility(@NonNull Visibility visibility) {
 }
 
 /**
- * {@linkplain Enum Enumeration} of all {@link Visibility} modifiers a {@link Widget} may have.
+ * @param width non-zero positive width of this {@link Widget}, or {@link #FILL_PARENT} or
+ *              {@link #WRAP_CONTENT} to represent a dynamic width which is assigned upon
+ *              {@linkplain WidgetParent#requestLayout() layout} of the {@linkplain #getParent()}
+ *              of this {@link Widget}.
  */
-public enum Visibility {
-    /** {@link Widget} will be laid out and rendered normally */
-    VISIBLE,
-    /** {@link Widget} will be laid out normally, however it will not be drawn */
-    INVISIBLE,
-    /** {@link Widget} will neither be laid out or drawn (as if it is not even present) */
-    GONE
+@IntRange(from=FILL_PARENT)
+public void setLayoutWidth(int width) {
+    if (width < FILL_PARENT) {
+        throw new IllegalArgumentException(
+                "width should range from " + FILL_PARENT + " to Integer.MAX_VALUE");
+    }
+
+    /* TODO: Assign width flag if equal to {@link #FILL_PARENT} or {@link #WRAP_CONTENT}, otherwise
+     *       should width be assigned at all? Width in this sense is a disjoint boolean variable,
+     *       and not a literal assignment of the width of this component (i.e., a suggestion for the
+     *       parent container of this Widget). */
+}
+
+/**
+ * @param height non-zero positive height of this {@link Widget}, or {@link #FILL_PARENT} or
+ *              {@link #WRAP_CONTENT} to represent a dynamic height which is assigned upon
+ *              {@linkplain WidgetParent#requestLayout() layout} of the {@linkplain #getParent()}
+ *              of this {@link Widget}.
+ */
+@IntRange(from=FILL_PARENT)
+public void setLayoutHeight(int height) {
+    if (height < FILL_PARENT) {
+        throw new IllegalArgumentException(
+                "height should range from " + FILL_PARENT + " to Integer.MAX_VALUE");
+    }
+
+    /* TODO: Assign height flag if equal to {@link #FILL_PARENT} or {@link #WRAP_CONTENT}, otherwise
+     *       should height be assigned at all? Height in this sense is a disjoint boolean variable,
+     *       and not a literal assignment of the height of this component (i.e., a suggestion for
+     *       the parent container of this Widget). */
 }
 
 public boolean hasFocus() { throw new UnsupportedOperationException(); }
