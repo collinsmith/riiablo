81ef00f4ce222598f00b02ac25972686e93ab129
Collin Smith <collinsmith70@gmail.com>
2016-02-19 04:48:18 -0800

    Fixed issue with desktop gradle not loading correct libs

--- core/src/com/gmail/collinsmith70/unifi/layout/LinearLayout.java
+++ core/src/com/gmail/collinsmith70/unifi/layout/LinearLayout.java
@@ -1,9 +1,58 @@
 package com.gmail.collinsmith70.unifi.layout;
 
+import android.support.annotation.NonNull;
+
 import com.gmail.collinsmith70.unifi.widget.Widget;
 import com.gmail.collinsmith70.unifi.widget.WidgetGroup;
 
 public class LinearLayout extends WidgetGroup {
+
+public enum Orientation {
+    HORIZONTAL {
+        @Override
+        void layout(LinearLayout linearLayout) {
+            int left = linearLayout.getLeft();
+            for (Widget child : linearLayout.getChildren()) {
+                child.setTop(linearLayout.getTop());
+                child.setLeft(left);
+                left += child.getWidth();
+            }
+        }
+    },
+    VERTICAL {
+        @Override
+        void layout(LinearLayout linearLayout) {int top = linearLayout.getTop();
+            for (Widget child : linearLayout.getChildren()) {
+                child.setTop(top);
+                child.setLeft(linearLayout.getLeft());
+                top += child.getHeight();
+            }
+        }
+    };
+
+    abstract void layout(LinearLayout linearLayout);
+}
+
+@NonNull private Orientation orientation;
+
+@NonNull public Orientation getOrientation() {
+    return orientation;
+}
+public void setOrientation(@NonNull Orientation orientation) {
+    if (orientation == null) {
+        throw new IllegalArgumentException("orientation cannot be null");
+    }
+
+    this.orientation = orientation;
+}
+
+public LinearLayout() {
+    this(Orientation.HORIZONTAL);
+}
+
+public LinearLayout(Orientation orientation) {
+    setOrientation(orientation);
+}
 
 @Override
 public boolean keyDown(int keycode) {
@@ -37,10 +86,7 @@ public boolean scrolled(int amount) {
 
 @Override
 public void requestLayout() {
-    int top = 0;
-    for (Widget child : getChildren()) {
-        child.setBounds(top, getLeft(), getRight(), top);
-    }
+    getOrientation().layout(this);
 }
 
 }
