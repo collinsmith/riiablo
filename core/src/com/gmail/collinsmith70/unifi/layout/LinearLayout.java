3dc69adc31d5e2fdee5e95c03663a74ce3283551
Collin Smith <collinsmith70@gmail.com>
2016-02-28 02:57:39 -0800

    Removed override for touchDragged

--- core/src/com/gmail/collinsmith70/unifi/layout/LinearLayout.java
+++ core/src/com/gmail/collinsmith70/unifi/layout/LinearLayout.java
@@ -152,9 +152,6 @@ public LinearLayout(Orientation orientation, Direction direction) {
 @Override public boolean keyTyped(char character) {
     return false;
 }
-@Override public boolean touchDragged(int screenX, int screenY, int pointer) {
-    return false;
-}
 @Override public boolean scrolled(int amount) {
     return false;
 }
