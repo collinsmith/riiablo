ec4a7ad3a1712ca33b8a78476acb86ac932a8703
Collin Smith <collinsmith70@gmail.com>
2016-02-29 15:17:10 -0800

    Added drawing of filled rectangles when in debug mode

--- core/src/com/gmail/collinsmith70/unifi/widget/Button.java
+++ core/src/com/gmail/collinsmith70/unifi/widget/Button.java
@@ -1,9 +1,42 @@
 package com.gmail.collinsmith70.unifi.widget;
 
+import android.support.annotation.NonNull;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
 public class Button extends Widget {
 
 public Button() {
     setFocusable(true);
+}
+
+@Override
+public void onDrawDebug(@NonNull Batch batch) {
+    if (batch == null) {
+        throw new IllegalArgumentException("batch should never be null");
+    }
+
+    final ShapeRenderer shapeRenderer = new ShapeRenderer();
+    shapeRenderer.begin(ShapeRenderer.ShapeType.Filled); {
+        Color color = Color.RED;
+        if (!isEnabled()) {
+            color = Color.DARK_GRAY;
+        } else if (!isFocusable()) {
+            color = Color.LIGHT_GRAY;
+        } else if (isDown() && isOver()) {
+            color = Color.GREEN;
+        } else if (isOver()) {
+            color = Color.BLUE;
+        }
+
+        shapeRenderer.setColor(color);
+        shapeRenderer.rect(getX()+1, getY()+1, getWidth(), getHeight());
+    } shapeRenderer.end();
+    shapeRenderer.dispose();
+
+    super.onDrawDebug(batch);
 }
 
 }
