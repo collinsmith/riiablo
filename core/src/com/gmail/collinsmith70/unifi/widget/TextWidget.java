7d3cb3a592c7fcb859784a5f95977f288997da32
Collin Smith <collinsmith70@gmail.com>
2016-03-02 15:18:51 -0800

    Added BitmapFont support (needed by libGDX) Added support for rendering the TextWidget's content

--- core/src/com/gmail/collinsmith70/unifi/widget/TextWidget.java
+++ core/src/com/gmail/collinsmith70/unifi/widget/TextWidget.java
@@ -1,30 +1,49 @@
 package com.gmail.collinsmith70.unifi.widget;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.google.common.base.Strings;
 
 public class TextWidget extends Widget {
 
-private String text;
-
-public TextWidget() {
-    this(null);
+public TextWidget(@NonNull BitmapFont font) {
+    this(font, null);
 }
-
-public TextWidget(String text) {
+public TextWidget(@NonNull BitmapFont font, @Nullable String text) {
+    setFont(font);
     setText(text);
 }
 
-public String getText() {
+@NonNull private String text;
+@NonNull public String getText() {
     return text;
 }
-
-public void setText(String text) {
+public void setText(@Nullable String text) {
     this.text = Strings.nullToEmpty(text);
 }
 
-@Override
-public String toString() {
+@NonNull private BitmapFont font;
+@NonNull public BitmapFont getFont() {
+    return font;
+}
+public void setFont(@NonNull BitmapFont font) {
+    if (font == null) {
+        throw new IllegalArgumentException("font cannot be null");
+    }
+
+    this.font = font;
+}
+
+@Override public String toString() {
     return text;
+}
+
+@Override
+public void onDraw(@NonNull Batch batch) {
+    getFont().draw(batch, getText(), getX(), getY());
 }
 
 }
