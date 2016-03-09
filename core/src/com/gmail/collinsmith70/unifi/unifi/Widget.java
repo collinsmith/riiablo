cc6ae82e44d532ccee59f751c45ae4a8a4edfc42
Collin Smith <collinsmith70@gmail.com>
2016-03-08 17:42:01 -0800

    Refactoring Bounded interface implementations to fit a better design goal (namely better enforced parameter validations)

--- core/src/com/gmail/collinsmith70/unifi/unifi/Widget.java
+++ core/src/com/gmail/collinsmith70/unifi/unifi/Widget.java
@@ -1,105 +1,58 @@
 package com.gmail.collinsmith70.unifi.unifi;
 
+import android.support.annotation.CallSuper;
 import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.gmail.collinsmith70.unifi.unifi.math.Boundary;
 import com.gmail.collinsmith70.unifi.unifi.math.Dimension2D;
-import com.gmail.collinsmith70.unifi.unifi.math.IntPoint2D;
+import com.gmail.collinsmith70.unifi.unifi.math.ImmutableBoundary;
+import com.gmail.collinsmith70.unifi.unifi.math.Point2D;
 
 public class Widget
         implements Bounded, Paddable, Parentable<WidgetParent>, Translateable {
 
-  @Nullable
-  private WidgetParent parent;
+  @Nullable private WidgetParent parent;
 
-  protected int bottom;
-  protected int left;
-  protected int right;
-  protected int top;
+  private int bottom;
+  private int left;
+  private int right;
+  private int top;
 
-  protected int paddingBottom;
-  protected int paddingLeft;
-  protected int paddingRight;
-  protected int paddingTop;
+  @IntRange(from = 0, to = Integer.MAX_VALUE) private int paddingBottom;
+  @IntRange(from = 0, to = Integer.MAX_VALUE) private int paddingLeft;
+  @IntRange(from = 0, to = Integer.MAX_VALUE) private int paddingRight;
+  @IntRange(from = 0, to = Integer.MAX_VALUE) private int paddingTop;
 
   public Widget() {
-
   }
 
-  @Override
-  @Nullable
-  public WidgetParent getParent() {
-    return parent;
-  }
-
-  final void setParent(@Nullable final WidgetParent parent) {
-    this.parent = parent;
-  }
-
-  @Override
-  public boolean hasParent() {
-    return getParent() != null;
-  }
-
-  public int getTranslationX() {
-    throw new UnsupportedOperationException();
-  }
-
-  public void setTranslationX(int x) {
-    throw new UnsupportedOperationException();
-  }
-
-  public int getTranslationY() {
-    throw new UnsupportedOperationException();
-  }
-
-  public void setTranslationY(int y) {
-    throw new UnsupportedOperationException();
-  }
-
-  @Override
-  public void setTranslation(int x, int y) {
-    throw new UnsupportedOperationException();
-  }
-
-  public void setTranslation(@NonNull final IntPoint2D src) {
-    throw new UnsupportedOperationException();
-  }
-
-  @NonNull
-  public IntPoint2D getTranslation() {
-    throw new UnsupportedOperationException();
-  }
-
-  @NonNull
-  public IntPoint2D getTranslation(@Nullable IntPoint2D dst) {
-    throw new UnsupportedOperationException();
-  }
-
+  /**
+   * Bottom position of this {@code Widget} relative to its {@linkplain #getParent parent}.
+   *
+   * @return Bottom of this {@code Widget}, in pixels
+   */
   @Override
   public final int getBottom() {
     return bottom;
   }
 
+  /**
+   * Sets the bottom position of this {@code Widget} relative to its {@linkplain #getParent parent}.
+   * <p>
+   *   Note: If the specified bottom is greater than the current {@linkplain #getTop top} of this
+   *         {@code Widget}, then the top is set to the same value as the specified bottom.
+   * </p>
+   * <p>
+   *   Note: This method is meant to be called by the layout system and should not generally be
+   *         called otherwise, because the property may be changed at any time by the layout.
+   * </p>
+   *
+   * @param bottom Bottom of this {@code Widget}, in pixels.
+   */
   @Override
-  public final int getLeft() {
-    return left;
-  }
-
-  @Override
-  public final int getRight() {
-    return right;
-  }
-
-  @Override
-  public final int getTop() {
-    return top;
-  }
-
-  @Override
-  public final void setBottom(int bottom) {
+  public final void setBottom(final int bottom) {
     if (getTop() < bottom) {
       this.top = bottom;
     }
@@ -107,8 +60,31 @@ public class Widget
     this.bottom = bottom;
   }
 
+  /**
+   * Left position of this {@code Widget} relative to its {@linkplain #getParent parent}.
+   *
+   * @return Left of this {@code Widget}, in pixels
+   */
   @Override
-  public final void setLeft(int left) {
+  public final int getLeft() {
+    return left;
+  }
+
+  /**
+   * Sets the left position of this {@code Widget} relative to its {@linkplain #getParent parent}.
+   * <p>
+   *   Note: If the specified left is greater than the current {@linkplain #getRight right} of this
+   *         {@code Widget}, then the right is set to the same value as the specified left.
+   * </p>
+   * <p>
+   *   Note: This method is meant to be called by the layout system and should not generally be
+   *         called otherwise, because the property may be changed at any time by the layout.
+   * </p>
+   *
+   * @param left Left of this {@code Widget}, in pixels.
+   */
+  @Override
+  public final void setLeft(final int left) {
     if (getRight() < left) {
       this.right = left;
     }
@@ -116,30 +92,125 @@ public class Widget
     this.left = left;
   }
 
+  /**
+   * Right position of this {@code Widget} relative to its {@linkplain #getParent parent}.
+   *
+   * @return Right of this {@code Widget}, in pixels
+   */
   @Override
-  public final void setRight(int right) {
-    if (getLeft() > right) {
+  public final int getRight() {
+    return right;
+  }
+
+  /**
+   * Sets the right position of this {@code Widget} relative to its {@linkplain #getParent parent}.
+   * <p>
+   *   Note: If the specified right is less than the current {@linkplain #getLeft left} of this
+   *         {@code Widget}, then the left is set to the same value as the specified right.
+   * </p>
+   * <p>
+   *   Note: This method is meant to be called by the layout system and should not generally be
+   *         called otherwise, because the property may be changed at any time by the layout.
+   * </p>
+   *
+   * @param right Right of this {@code Widget}, in pixels.
+   */
+  @Override
+  public final void setRight(final int right) {
+    if (right < getLeft()) {
       this.left = right;
     }
 
     this.right = right;
   }
 
+  /**
+   * Top position of this {@code Widget} relative to its {@linkplain #getParent parent}.
+   *
+   * @return Top of this {@code Widget}, in pixels
+   */
   @Override
-  public final void setTop(int top) {
-    if (getBottom() > top) {
+  public final int getTop() {
+    return top;
+  }
+
+  /**
+   * Sets the top position of this {@code Widget} relative to its {@linkplain #getParent parent}.
+   * <p>
+   *   Note: If the specified top is less than the current {@linkplain #getBottom bottom} of this
+   *         {@code Widget}, then the bottom is set to the same value as the specified top.
+   * </p>
+   * <p>
+   *   Note: This method is meant to be called by the layout system and should not generally be
+   *         called otherwise, because the property may be changed at any time by the layout.
+   * </p>
+   *
+   * @param top Top of this {@code Widget}, in pixels.
+   */
+  @Override
+  public final void setTop(final int top) {
+    if (top < getBottom()) {
       this.bottom = top;
     }
 
     this.top = top;
   }
 
+  /**
+   * {@link Boundary} containing of the positions of this {@code Widget} relative to its
+   * {@linkplain #getParent parent}.
+   * <p>
+   *   Note: Changing the sides of the returned {@code Boundary} instance will not be reflected
+   *         within this {@code Widget}.
+   * </p>
+   *
+   * @return {@code Boundary} containing the positions of this {@code Widget}
+   */
+  @NonNull
   @Override
-  public final void setBoundary(int left, int top, int right, int bottom) {
+  public final Boundary getBoundary() {
+    return new Boundary(getLeft(), getTop(), getRight(), getBottom());
+  }
+
+  /**
+   * Populates the passed {@link Boundary} instance with the positions of this {@code Widget}
+   * relative to its {@linkplain #getParent parent}.
+   *
+   * @param dst {@code Boundary} instance to populate, otherwise if a {@code null} reference is
+   *            passed, then this method would behave the same as if {@link #getBoundary} were
+   *            called.
+   *
+   * @return {@code Boundary} containing the positions of this {@code Widget}
+   */
+  @NonNull
+  @Override
+  public final Boundary getBoundary(@Nullable final Boundary dst) {
+    if (dst == null) {
+      return getBoundary();
+    }
+
+    dst.set(getLeft(), getTop(), getRight(), getBottom());
+    return dst;
+  }
+
+  /**
+   * Sets the positions of all sides of this {@code Widget} relative to its
+   * {@linkplain #getParent parent}.
+   * <p>
+   *   Precondition: {@code left <= right AND bottom <= top}
+   * </p>
+   *
+   * @param left   Left of this {@code Widget}, in pixels.
+   * @param top    Top of this {@code Widget}, in pixels.
+   * @param right  Right of this {@code Widget}, in pixels.
+   * @param bottom Bottom of this {@code Widget}, in pixels.
+   */
+  @Override
+  public final void setBoundary(final int left, final int top, final int right, final int bottom) {
     if (right < left) {
-      throw new IllegalArgumentException("right should be greater than or equal to left");
+      throw new IllegalArgumentException("left <= right");
     } else if (top < bottom) {
-      throw new IllegalArgumentException("top should be greater than or equal to bottom");
+      throw new IllegalArgumentException("bottom <= top");
     }
 
     this.left = left;
@@ -148,255 +219,77 @@ public class Widget
     this.bottom = bottom;
   }
 
+  /**
+   * Sets the positions of all sides of this {@code Widget} relative to its
+   * {@linkplain #getParent parent} from the specified source {@link Boundary}.
+   * <p>
+   *   Precondition: {@code src.getLeft() <= src.getRight() AND src.getBottom() <= src.getTop()}
+   * </p>
+   *
+   * @param src {@code Boundary} to set the side positions of this {@code Widget} to
+   */
   @Override
   public final void setBoundary(@NonNull final Boundary src) {
     if (src == null) {
-      throw new IllegalArgumentException("src boundary cannot be null");
+      throw new IllegalArgumentException("src cannot be null");
     }
 
     setBoundary(src.getLeft(), src.getTop(), src.getRight(), src.getBottom());
   }
 
+  /**
+   * Width of this {@code Widget}
+   *
+   * @return Width of this {@code Widget}, in pixels
+   */
   @Override
-  @NonNull
-  public final Boundary getBoundary() {
-    return new Boundary(left, top, right, bottom);
+  @IntRange(from = 0, to = Integer.MAX_VALUE)
+  public final int getWidth() {
+    return getRight() - getLeft();
   }
 
-  @NonNull
+  /**
+   * Height of this {@code Widget}
+   *
+   * @return Height of this {@code Widget}, in pixels
+   */
   @Override
-  public final Boundary getBoundary(@Nullable Boundary dst) {
-    if (dst == null) {
-      return getBoundary();
-    }
-
-    dst.set(left, top, right, bottom);
-    return dst;
+  @IntRange(from = 0, to = Integer.MAX_VALUE)
+  public final int getHeight() {
+    return getTop() - getBottom();
   }
 
-  @Override
-  public final boolean contains(int x, int y) {
-    x -= getX();
-    y -= getY();
-    return 0 <= x && x <= getWidth()
-            && 0 <= y && y <= getHeight();
-  }
-
-  @Override
-  public final void setPosition(int x, int y) {
-    setX(x);
-    setY(y);
-  }
-
-  @Override
-  public final void setPosition(@NonNull final IntPoint2D src) {
-    if (src == null) {
-      throw new IllegalArgumentException("src point cannot be null");
-    }
-
-    setPosition(src.getX(), src.getY());
-  }
-
-  @Override
-  @NonNull
-  public final IntPoint2D getPosition() {
-    return new IntPoint2D(getX(), getY());
-  }
-
+  /**
+   * {@link Dimension2D} containing the {@linkplain #getWidth width} and
+   * {@linkplain #getHeight height} dimensions of this {@code Widget}.
+   *
+   * @return {@code Dimension2D} containing the dimensions of this {@code Widget}
+   */
   @NonNull
   @Override
-  public final IntPoint2D getPosition(@Nullable IntPoint2D dst) {
-    if (dst == null) {
-      return getPosition();
-    }
-
-    dst.set(getX(), getY());
-    return dst;
-  }
-
-  @Override
-  public final void setSize(@IntRange(from = 0, to = Integer.MAX_VALUE) int width,
-                            @IntRange(from = 0, to = Integer.MAX_VALUE) int height) {
-    setWidth(width);
-    setHeight(height);
-  }
-
-  @Override
-  public final void setSize(@NonNull final Dimension2D src) {
-    if (src == null) {
-      throw new IllegalArgumentException("src dimension cannot be null");
-    }
-
-    setSize(src.getWidth(), src.getHeight());
-  }
-
-  @Override
-  @NonNull
   public final Dimension2D getSize() {
     return new Dimension2D(getWidth(), getHeight());
   }
 
+  /**
+   * Populates the passed {@link Dimension2D} instance with the {@linkplain #getWidth width} and
+   * {@linkplain #getHeight height} dimensions of this {@code Widget}.
+   *
+   * @param dst {@code Dimension2D} instance to populate, otherwise if a {@code null} reference is
+   *            passed, then this method would behave the same as if {@link #getSize} were
+   *            called.
+   *
+   * @return {@code Dimension2D} containing the dimensions of this {@code Widget}
+   */
   @NonNull
   @Override
-  public final Dimension2D getSize(@Nullable Dimension2D dst) {
+  public Dimension2D getSize(@Nullable final Dimension2D dst) {
     if (dst == null) {
       return getSize();
     }
 
     dst.set(getWidth(), getHeight());
     return dst;
-  }
-
-  @Override
-  public final boolean hasSize() {
-    return left != right && top != bottom;
-  }
-
-  @Override
-  public int getX() {
-    return left + getTranslationX();
-  }
-
-  @Override
-  public int getY() {
-    return bottom + getTranslationY();
-  }
-
-  @Override
-  public int getWidth() {
-    return right - left;
-  }
-
-  @Override
-  public int getHeight() {
-    return top - bottom;
-  }
-
-  @Override
-  public void setX(int x) {
-    setTranslationX(x - left);
-  }
-
-  @Override
-  public void setY(int y) {
-    setTranslationY(y - bottom);
-  }
-
-  @Override
-  public void setWidth(@IntRange(from = 0, to = Integer.MAX_VALUE) int width) {
-    if (width < 0) {
-      throw new IllegalArgumentException("width should be greater than or equal to 0");
-    }
-
-    this.right = left + width;
-  }
-
-  @Override
-  public void setHeight(@IntRange(from = 0, to = Integer.MAX_VALUE) int height) {
-    if (height < 0) {
-      throw new IllegalArgumentException("height should be greater than or equal to 0");
-    }
-
-    this.top = bottom + height;
-  }
-
-  @Override
-  public int getPaddingBottom() {
-    return paddingBottom;
-  }
-
-  @Override
-  public int getPaddingLeft() {
-    return paddingLeft;
-  }
-
-  @Override
-  public int getPaddingRight() {
-    return paddingRight;
-  }
-
-  @Override
-  public int getPaddingTop() {
-    return paddingTop;
-  }
-
-  @Override
-  public void setPaddingBottom(@IntRange(from = 0, to = Integer.MAX_VALUE) int bottom) {
-    if (bottom < 0) {
-      throw new IllegalArgumentException("bottom padding should be a natural number");
-    }
-
-    this.paddingBottom = bottom;
-  }
-
-  @Override
-  public void setPaddingLeft(@IntRange(from = 0, to = Integer.MAX_VALUE) int left) {
-    if (left < 0) {
-      throw new IllegalArgumentException("left padding should be a natural number");
-    }
-
-    this.paddingLeft = left;
-  }
-
-  @Override
-  public void setPaddingRight(@IntRange(from = 0, to = Integer.MAX_VALUE) int right) {
-    if (right < 0) {
-      throw new IllegalArgumentException("right padding should be a natural number");
-    }
-
-    this.paddingRight = right;
-  }
-
-  @Override
-  public void setPaddingTop(@IntRange(from = 0, to = Integer.MAX_VALUE) int top) {
-    if (top < 0) {
-      throw new IllegalArgumentException("top padding should be a natural number");
-    }
-
-    this.paddingTop = top;
-  }
-
-  @Override
-  public void setPadding(@IntRange(from = 0, to = Integer.MAX_VALUE) int left,
-                         @IntRange(from = 0, to = Integer.MAX_VALUE) int top,
-                         @IntRange(from = 0, to = Integer.MAX_VALUE) int right,
-                         @IntRange(from = 0, to = Integer.MAX_VALUE) int bottom) {
-    setPaddingBottom(bottom);
-    setPaddingLeft(left);
-    setPaddingRight(right);
-    setPaddingTop(top);
-  }
-
-  @Override
-  public void setPadding(@NonNull final Boundary src) {
-    if (src == null) {
-      throw new IllegalArgumentException("src padding cannot be null");
-    }
-
-    setPadding(src.getLeft(), src.getTop(), src.getRight(), src.getBottom());
-  }
-
-  @Override
-  @NonNull
-  public Boundary getPadding() {
-    return new Boundary(paddingLeft, paddingTop, paddingRight, paddingBottom);
-  }
-
-  @NonNull
-  @Override
-  public Boundary getPadding(@Nullable Boundary dst) {
-    if (dst == null) {
-      return getPadding();
-    }
-
-    dst.set(paddingLeft, paddingTop, paddingRight, paddingBottom);
-    return dst;
-  }
-
-  @Override
-  public boolean hasPadding() {
-    return paddingLeft > 0 || paddingRight > 0
-            || paddingBottom > 0 || paddingTop > 0;
   }
 
 }
