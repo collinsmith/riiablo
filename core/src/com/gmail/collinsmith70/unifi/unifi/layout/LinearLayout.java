daa8f64c934357db9dcc65f3978135ab3a0a81e5
Collin Smith <collinsmith70@gmail.com>
2016-03-13 17:24:18 -0700

    Added LinearLayout class

---
+++ core/src/com/gmail/collinsmith70/unifi/unifi/layout/LinearLayout.java
@@ -0,0 +1,12 @@
+package com.gmail.collinsmith70.unifi.unifi.layout;
+
+import com.gmail.collinsmith70.unifi.unifi.WidgetGroup;
+
+public class LinearLayout extends WidgetGroup {
+
+  public enum Direction {
+    HORIZONTAL,
+    VERTICAL
+  }
+
+}
