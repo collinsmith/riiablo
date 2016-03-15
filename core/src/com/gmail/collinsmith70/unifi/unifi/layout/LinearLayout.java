d46115ea9281070c334955802fae761d2fd7de35
Collin Smith <collinsmith70@gmail.com>
2016-03-15 15:29:47 -0700

    Added HorizontalLayout which is a subclass of LinearLayout Added spacing field to LinearLayout (spacing between components)

--- core/src/com/gmail/collinsmith70/unifi/unifi/layout/LinearLayout.java
+++ core/src/com/gmail/collinsmith70/unifi/unifi/layout/LinearLayout.java
@@ -1,12 +1,24 @@
 package com.gmail.collinsmith70.unifi.unifi.layout;
 
+import android.support.annotation.IntRange;
+
 import com.gmail.collinsmith70.unifi.unifi.WidgetGroup;
 
 public class LinearLayout extends WidgetGroup {
 
-  public enum Direction {
-    HORIZONTAL,
-    VERTICAL
+  @IntRange(from = 0, to = Integer.MAX_VALUE) private int spacing;
+
+  @IntRange(from = 0, to = Integer.MAX_VALUE)
+  public int getSpacing() {
+    return spacing;
+  }
+
+  public void setSpacing(@IntRange(from = 0, to = Integer.MAX_VALUE) int spacing) {
+    if (spacing < 0) {
+      throw new IllegalArgumentException("spacing must be greater than or equal to 0");
+    }
+
+    this.spacing = spacing;
   }
 
 }
