7c3de9bded2c2995230ef5492aa1cf6544708ee1
Collin Smith <collinsmith70@gmail.com>
2016-03-19 04:20:53 -0700

    Documented getSpacing and setSpacing

--- core/src/com/gmail/collinsmith70/unifi/unifi/layout/LinearLayout.java
+++ core/src/com/gmail/collinsmith70/unifi/unifi/layout/LinearLayout.java
@@ -4,15 +4,25 @@ import android.support.annotation.IntRange;
 
 import com.gmail.collinsmith70.unifi.unifi.WidgetGroup;
 
-public class LinearLayout extends WidgetGroup {
+public abstract class LinearLayout extends WidgetGroup {
 
   @IntRange(from = 0, to = Integer.MAX_VALUE) private int spacing;
 
+  /**
+   * Spacing between the children of this {@code LinearLayout}.
+   *
+   * @return Spacing between children of this {@code LinearLayout}, in pixels
+   */
   @IntRange(from = 0, to = Integer.MAX_VALUE)
   public int getSpacing() {
     return spacing;
   }
 
+  /**
+   * Sets the spacing between the children of this {@code LinearLayout}.
+   *
+   * @param spacing Spacing between children of this {@code LinearLayout}, in pixels
+   */
   public void setSpacing(@IntRange(from = 0, to = Integer.MAX_VALUE) int spacing) {
     if (spacing < 0) {
       throw new IllegalArgumentException("spacing must be greater than or equal to 0");
