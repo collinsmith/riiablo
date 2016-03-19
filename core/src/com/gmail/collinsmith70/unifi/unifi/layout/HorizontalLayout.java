2ace853983bb9f243790735026f9ea0f553839ec
Collin Smith <collinsmith70@gmail.com>
2016-03-19 04:25:19 -0700

    Implemented requestLayout

--- core/src/com/gmail/collinsmith70/unifi/unifi/layout/HorizontalLayout.java
+++ core/src/com/gmail/collinsmith70/unifi/unifi/layout/HorizontalLayout.java
@@ -11,11 +11,19 @@ public class HorizontalLayout extends LinearLayout {
     for (Widget child : this) {
 
       if ((gravity&CENTER_VERTICAL) != 0) {
-        child.translateVerticalCenter(getPaddingLeft());
+        child.translateVerticalCenter((getHeight() - getPaddingTop() - getPaddingBottom()) / 2);
       } else if ((gravity&BOTTOM) != 0) {
-        child.translateBottom(0);
+        child.translateBottom(getPaddingBottom());
       } else {
-        child.translateTop(getHeight());
+        child.translateTop(getHeight() - getPaddingTop());
+      }
+
+      if ((gravity&CENTER_HORIZONTAL) != 0) {
+        child.translateHorizontalCenter((getWidth() - getPaddingLeft() - getPaddingRight()) / 2);
+      } else if ((gravity&RIGHT) != 0) {
+        child.translateRight(getWidth() - getPaddingRight());
+      } else {
+        child.translateLeft(getPaddingLeft());
       }
 
     }
