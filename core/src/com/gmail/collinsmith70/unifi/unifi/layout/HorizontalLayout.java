a3114591de8f6f5ee88e51355160621356396bc5
Collin Smith <collinsmith70@gmail.com>
2016-03-19 04:21:21 -0700

    Refactored out Gravities, which was removed

--- core/src/com/gmail/collinsmith70/unifi/unifi/layout/HorizontalLayout.java
+++ core/src/com/gmail/collinsmith70/unifi/unifi/layout/HorizontalLayout.java
@@ -1,5 +1,24 @@
 package com.gmail.collinsmith70.unifi.unifi.layout;
 
+import com.gmail.collinsmith70.unifi.unifi.Widget;
+
 public class HorizontalLayout extends LinearLayout {
+
+  @Override
+  public void requestLayout() {
+
+    @Gravity final int gravity = getGravity();
+    for (Widget child : this) {
+
+      if ((gravity&CENTER_VERTICAL) != 0) {
+        child.translateVerticalCenter(getPaddingLeft());
+      } else if ((gravity&BOTTOM) != 0) {
+        child.translateBottom(0);
+      } else {
+        child.translateTop(getHeight());
+      }
+
+    }
+  }
 
 }
