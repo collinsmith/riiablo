efd22a00d2625befa38f1fa1bf885ead969eaf31
Collin Smith <collinsmith70@gmail.com>
2016-03-23 19:58:18 -0700

    Transitioned from long to double representation (will be cleaner when moving from metric to imperial). Devices measure pixel density in DPI (dots per inch), while this system will use both.

--- core/src/com/gmail/collinsmith70/unifi/unifi/util/LengthUnit.java
+++ core/src/com/gmail/collinsmith70/unifi/unifi/util/LengthUnit.java
@@ -18,31 +18,32 @@ import com.badlogic.gdx.Gdx;
 public enum LengthUnit {
 
   MILLIMETERS {
-    @Override public long toMillimeters(long l)                  { return l; }
-    @Override public long toCentimeters(long l)                  { return l/(C1/C0); }
-    @Override public long toMeters(long l)                       { return l/(C2/C0); }
-    @Override public long convert(long l, @NonNull LengthUnit u) { return u.toMillimeters(l); }
+    @Override public double toMillimeters(double l)                  { return l; }
+    @Override public double toCentimeters(double l)                  { return l/(C1/C0); }
+    @Override public double toMeters(double l)                       { return l/(C2/C0); }
+    @Override public double convert(double l, @NonNull LengthUnit u) { return u.toMillimeters(l); }
   },
 
   CENTIMETERS {
-    @Override public long toMillimeters(long l)                  { return x(l, C1/C0, MAX/(C1/C0)); }
-    @Override public long toCentimeters(long l)                  { return l; }
-    @Override public long toMeters(long l)                       { return l/(C2/C1); }
-    @Override public long convert(long l, @NonNull LengthUnit u) { return u.toCentimeters(l); }
+    @Override public double toMillimeters(double l)                  { return x(l, C1/C0, MAX/(C1/C0)); }
+    @Override public double toCentimeters(double l)                  { return l; }
+    @Override public double toMeters(double l)                       { return l/(C2/C1); }
+    @Override public double convert(double l, @NonNull LengthUnit u) { return u.toCentimeters(l); }
   },
 
   METERS {
-    @Override public long toMillimeters(long l)                  { return x(l, C2/C0, MAX/(C2/C0)); }
-    @Override public long toCentimeters(long l)                  { return x(l, C2/C1, MAX/(C2/C1)); }
-    @Override public long toMeters(long l)                       { return l; }
-    @Override public long convert(long l, @NonNull LengthUnit u) { return u.toMeters(l); }
+    @Override public double toMillimeters(double l)                  { return x(l, C2/C0, MAX/(C2/C0)); }
+    @Override public double toCentimeters(double l)                  { return x(l, C2/C1, MAX/(C2/C1)); }
+    @Override public double toMeters(double l)                       { return l; }
+    @Override public double convert(double l, @NonNull LengthUnit u) { return u.toMeters(l); }
   };
 
-  private static final long C0 = 1L;
-  private static final long C1 = C0 * 100L;
-  private static final long C2 = C0 * 1000L;
+  private static final double C0 = 1.0;
+  private static final double C1 = C0 * 100.0;
+  private static final double C2 = C0 * 1000.0;
 
-  private static final long MAX = Long.MAX_VALUE;
+  private static final double MAX = Double.MAX_VALUE;
+  private static final double PPC = Gdx.graphics.getDensity() * 2.54;
 
   /**
    * Number of pixels on the screen which equals the corresponding length and {@code LengthUnit}.
@@ -168,7 +169,7 @@ public enum LengthUnit {
    *
    * @return Number of pixels corresponding with that length and unit
    */
-  public static long toPixels(long sourceLength, LengthUnit sourceUnit) {
+  public static int toPixels(double sourceLength, LengthUnit sourceUnit) {
     return toPixelsX(sourceLength, sourceUnit);
   }
 
@@ -181,11 +182,11 @@ public enum LengthUnit {
    *
    * @return Number of pixels corresponding with that length and unit
    *
-   * @see #toPixelsY(long, LengthUnit)
+   * @see #toPixelsY(double, LengthUnit)
    */
-  public static long toPixelsX(long sourceLength, LengthUnit sourceUnit) {
-    long mms = sourceUnit.toMillimeters(sourceLength);
-    return (long)(Gdx.graphics.getPpcX() * (double)mms/10);
+  public static int toPixelsX(double sourceLength, LengthUnit sourceUnit) {
+    double cms = sourceUnit.toCentimeters(sourceLength);
+    return (int)Math.round(PPC * cms);
   }
 
   /**
@@ -197,22 +198,22 @@ public enum LengthUnit {
    *
    * @return Number of pixels corresponding with that length and unit
    *
-   * @see #toPixelsX(long, LengthUnit)
+   * @see #toPixelsX(double, LengthUnit)
    */
-  public static long toPixelsY(long sourceLength, LengthUnit sourceUnit) {
-    long mms = sourceUnit.toMillimeters(sourceLength);
-    return (long)(Gdx.graphics.getPpcY() * (double)mms/10);
+  public static int toPixelsY(double sourceLength, LengthUnit sourceUnit) {
+    double cms = sourceUnit.toCentimeters(sourceLength);
+    return (int)Math.round(PPC * cms);
   }
 
   /**
    * Scale l by m, checking for overflow. This has a short name to make above code more readable.
    * <p>Note: Taken from {@link java.util.concurrent.TimeUnit#x(long, long, long)}</p>
    */
-  static long x(long l, long m, long over) {
+  private static double x(double l, double m, double over) {
     if (l > over) {
-      return Long.MAX_VALUE;
+      return Double.MAX_VALUE;
     } else if (l < -over) {
-      return Long.MIN_VALUE;
+      return Double.MIN_VALUE;
     }
 
     return l * m;
@@ -224,9 +225,9 @@ public enum LengthUnit {
    * @param length length to convert
    * @return converted length, or {@link Long#MIN_VALUE} if conversion would negatively overflow, or
    * {@link Long#MAX_VALUE} if it would positively overflow
-   * @see #convert(long, LengthUnit)
+   * @see #convert(double, LengthUnit)
    */
-  public abstract long toMillimeters(long length);
+  public abstract double toMillimeters(double length);
 
   /**
    * Equivalent to {@code CENTIMETERS.convert(length, this)}.
@@ -234,9 +235,9 @@ public enum LengthUnit {
    * @param length length to convert
    * @return converted length, or {@link Long#MIN_VALUE} if conversion would negatively overflow, or
    * {@link Long#MAX_VALUE} if it would positively overflow
-   * @see #convert(long, LengthUnit)
+   * @see #convert(double, LengthUnit)
    */
-  public abstract long toCentimeters(long length);
+  public abstract double toCentimeters(double length);
 
   /**
    * Equivalent to {@code METERS.convert(length, this)}.
@@ -244,9 +245,9 @@ public enum LengthUnit {
    * @param length length to convert
    * @return converted length, or {@link Long#MIN_VALUE} if conversion would negatively overflow, or
    * {@link Long#MAX_VALUE} if it would positively overflow
-   * @see #convert(long, LengthUnit)
+   * @see #convert(double, LengthUnit)
    */
-  public abstract long toMeters(long length);
+  public abstract double toMeters(double length);
 
   /**
    * Converts the given length in the given unit to this unit. Conversions from finer to coarser
@@ -263,6 +264,6 @@ public enum LengthUnit {
    * @return converted duration in this unit, or {@link Long#MIN_VALUE} if conversion would negatively
    * overflow, or {@link Long#MAX_VALUE} if it would positively overflow
    */
-  public abstract long convert(long sourceLength, @NonNull LengthUnit sourceUnit);
+  public abstract double convert(double sourceLength, @NonNull LengthUnit sourceUnit);
 
 }
