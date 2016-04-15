ff955f43ccd571660e03b9250a79217b9e82ca47
Collin Smith <collinsmith70@gmail.com>
2016-04-14 21:58:57 -0700

    Added full support for Canvas clipping on all operations

--- core/src/com/gmail/collinsmith70/unifi/unifi/graphics/Canvas.java
+++ core/src/com/gmail/collinsmith70/unifi/unifi/graphics/Canvas.java
@@ -12,15 +12,20 @@ import com.gmail.collinsmith70.unifi.unifi.math.Boundary;
 
 public class Canvas implements Disposable {
 
+  private static final Pixmap.Format PIXMAP_FORMAT = Pixmap.Format.RGBA8888;
+
   @NonNull
   private Pixmap pixmap;
 
   @NonNull
   private Boundary clip;
 
+  @Nullable
+  private Pixmap clippedPixmap;
+
   public Canvas(@IntRange(from = 0, to = Integer.MAX_VALUE) final int width,
                 @IntRange(from = 0, to = Integer.MAX_VALUE) final int height) {
-    this(new Pixmap(width, height, Pixmap.Format.RGBA8888));
+    this(new Pixmap(width, height, PIXMAP_FORMAT));
   }
 
   public Canvas(@NonNull final Pixmap pixmap) {
@@ -58,18 +63,39 @@ public class Canvas implements Disposable {
   }
 
   public void clipRectangle(final int left, final int top, final int right, final int bottom) {
+    if (right < left) {
+      throw new IllegalArgumentException("left should be less than or equal to right");
+    } else if (bottom < top) {
+      throw new IllegalArgumentException("top should be less than or equal to bottom");
+    }
+
     clip.set(left, top, right, bottom);
   }
 
   public void clipRectangle(@NonNull final Boundary src) {
-    clip.set(src);
+    clipRectangle(src.getLeft(), src.getTop(), src.getRight(), src.getBottom());
   }
 
   private void prepare(@NonNull final Paint paint) {
+    if (clippedPixmap != null) {
+      throw new IllegalStateException("prepare called without calling finish");
+    }
+
+    this.clippedPixmap = new Pixmap(clip.getWidth(), clip.getHeight(), PIXMAP_FORMAT);
     Gdx.gl.glLineWidth(paint.getStrokeWidth());
-    pixmap.setColor(paint.getColor());
+    clippedPixmap.setColor(paint.getColor());
     Pixmap.setBlending(paint.getBlendingMode());
     Pixmap.setFilter(paint.getFilterMode());
+  }
+
+  private void finish() {
+    if (clippedPixmap == null) {
+      throw new IllegalStateException("finish called without calling prepare");
+    }
+
+    pixmap.drawPixmap(clippedPixmap, clip.getLeft(), clip.getTop());
+    clippedPixmap.dispose();
+    this.clippedPixmap = null;
   }
 
   public void drawLine(final int x,
@@ -89,13 +115,15 @@ public class Canvas implements Disposable {
                                @IntRange(from = 0, to = Integer.MAX_VALUE) final int length,
                                @NonNull final Paint paint) {
     prepare(paint);
-    pixmap.fillRectangle(x, y, paint.getStrokeWidth(), length);
+    clippedPixmap.fillRectangle(x - clip.getLeft(), y - clip.getTop(), paint.getStrokeWidth(), length);
+    finish();
   }
 
   public void drawHorizontalLine(final int x, final int y, final int length,
                                  @NonNull final Paint paint) {
     prepare(paint);
-    pixmap.fillRectangle(x, y, length, paint.getStrokeWidth());
+    clippedPixmap.fillRectangle(x - clip.getLeft(), y - clip.getTop(), length, paint.getStrokeWidth());
+    finish();
   }
 
   public void drawRectangle(final int x,
@@ -105,14 +133,16 @@ public class Canvas implements Disposable {
                             @NonNull final Paint paint) {
     prepare(paint);
     if (paint.getStyle() == Paint.Style.FILL) {
-      pixmap.fillRectangle(x, y, width, height);
+      clippedPixmap.fillRectangle(x - clip.getLeft(), y, width, height);
+      finish();
       return;
     }
 
-    pixmap.fillRectangle(x, y, paint.getStrokeWidth(), height);
-    pixmap.fillRectangle(x + width - paint.getStrokeWidth(), y, paint.getStrokeWidth(), height);
-    pixmap.fillRectangle(x, y + height - paint.getStrokeWidth(), width, paint.getStrokeWidth());
-    pixmap.fillRectangle(x, y, width, paint.getStrokeWidth());
+    clippedPixmap.fillRectangle(x - clip.getLeft(), y - clip.getTop(), paint.getStrokeWidth(), height);
+    clippedPixmap.fillRectangle(x + width - paint.getStrokeWidth() - clip.getLeft(), y - clip.getTop(), paint.getStrokeWidth(), height);
+    clippedPixmap.fillRectangle(x - clip.getLeft(), y + height - paint.getStrokeWidth() - clip.getTop(), width, paint.getStrokeWidth());
+    clippedPixmap.fillRectangle(x - clip.getLeft(), y - clip.getTop(), width, paint.getStrokeWidth());
+    finish();
   }
 
   public void drawCircle(final int x,
@@ -121,13 +151,16 @@ public class Canvas implements Disposable {
                          @NonNull final Paint paint) {
     prepare(paint);
     if (paint.getStyle() == Paint.Style.FILL) {
-      pixmap.fillCircle(x, y, radius);
+      clippedPixmap.fillCircle(x - clip.getLeft(), y - clip.getTop(), radius);
+      finish();
       return;
     }
 
     for (int i = 0; i < paint.getStrokeWidth(); i++) {
-      pixmap.drawCircle(x, y, radius - i);
+      clippedPixmap.drawCircle(x - clip.getLeft(), y - clip.getTop(), radius - i);
     }
+
+    finish();
   }
 
   public void drawCanvas(final int x,
@@ -141,7 +174,8 @@ public class Canvas implements Disposable {
                          @NonNull final Canvas canvas,
                          @NonNull final Paint paint) {
     prepare(paint);
-    pixmap.drawPixmap(canvas.getPixmap(), x, y);
+    clippedPixmap.drawPixmap(canvas.getPixmap(), x - clip.getLeft(), y - clip.getTop());
+    finish();
   }
 
   public void drawPixmap(final int x,
@@ -155,12 +189,14 @@ public class Canvas implements Disposable {
                          @NonNull final Pixmap pixmap,
                          @NonNull final Paint paint) {
     prepare(paint);
-    pixmap.drawPixmap(pixmap, x, y);
+    clippedPixmap.drawPixmap(pixmap, x - clip.getLeft(), y - clip.getTop());
+    finish();
   }
 
   public void fill(@NonNull final Paint paint) {
     prepare(paint);
-    pixmap.fill();
+    clippedPixmap.fill();
+    finish();
   }
 
   public Pixmap getPixmap() {
