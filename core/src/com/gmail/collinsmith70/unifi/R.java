3c3bc3a2202065ca0cc34edd85b152486d1d6df2
Collin Smith <collinsmith70@gmail.com>
2016-05-03 14:08:01 -0700

    Implemented theme method bodies with temp code for testing purposes

--- core/src/com/gmail/collinsmith70/unifi/R.java
+++ core/src/com/gmail/collinsmith70/unifi/R.java
@@ -14,11 +14,11 @@ public final class R {
         private styleable() {
         }
 
-        public static final AttributeDecl[] ColorDrawable = new AttributeDecl[] {
-                new AttributeDecl("color", UnsignedInteger.class)
-        };
-
         public static final int ColorDrawable_color = 0;
+
+        public static final AttributeDecl[] ColorDrawable = new AttributeDecl[] {
+                new AttributeDecl(ColorDrawable_color, "color", UnsignedInteger.class)
+        };
 
     }
 
