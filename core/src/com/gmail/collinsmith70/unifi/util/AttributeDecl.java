2a72b5ba4b4427ead8ace4016f4fd2d2640a3c8c
Collin Smith <collinsmith70@gmail.com>
2016-05-10 14:56:08 -0700

    Added support for generics and a default value reference

--- core/src/com/gmail/collinsmith70/unifi/util/AttributeDecl.java
+++ core/src/com/gmail/collinsmith70/unifi/util/AttributeDecl.java
@@ -2,31 +2,37 @@ package com.gmail.collinsmith70.unifi.util;
 
 import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import org.apache.commons.lang3.Validate;
 
 import java.util.Locale;
 
-public final class AttributeDecl {
+public final class AttributeDecl<T> {
 
     @NonNull
     private final String name;
 
     @NonNull
-    private final Class<?> valueType;
+    private final Class<T> valueType;
 
     @IntRange(from = 0, to = Integer.MAX_VALUE)
     private final int index;
 
+    @Nullable
+    private final T defaultValue;
+
     public AttributeDecl(@IntRange(from = 0, to = Integer.MAX_VALUE) int index,
                          @NonNull String name,
-                         @NonNull Class<?> valueType) {
+                         @NonNull Class<T> valueType,
+                         @Nullable T defaultValue) {
         Validate.isTrue(index >= 0, "index cannot be less than 0");
         Validate.isTrue(name != null, "name cannot be null");
         Validate.isTrue(valueType != null, "valueType cannot be null");
         this.index = index;
         this.name = name;
         this.valueType = valueType;
+        this.defaultValue = defaultValue;
     }
 
     @IntRange(from = 0, to = Integer.MAX_VALUE)
@@ -40,8 +46,13 @@ public final class AttributeDecl {
     }
 
     @NonNull
-    public Class<?> getValueType() {
+    public Class<T> getValueType() {
         return valueType;
+    }
+
+    @Nullable
+    public T getDefaultValue() {
+        return defaultValue;
     }
 
     @Override
