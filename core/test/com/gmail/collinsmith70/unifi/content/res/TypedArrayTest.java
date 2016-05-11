222eca9c75e1cd1ec203736c997a739c0dd7bf4e
Collin Smith <collinsmith70@gmail.com>
2016-05-11 15:00:17 -0700

    Fixed tests by adding assertThrows statements

--- core/test/com/gmail/collinsmith70/unifi/content/res/TypedArrayTest.java
+++ core/test/com/gmail/collinsmith70/unifi/content/res/TypedArrayTest.java
@@ -1,23 +1,47 @@
 package com.gmail.collinsmith70.unifi.content.res;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.gmail.collinsmith70.diablo.Client;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
 /**
  *
  * @author Collin Smith <i>collinsmith70@gmail.com</i>
  */
 public class TypedArrayTest {
-    
-    private final AssetManager ASSETS = new AssetManager();
-    
-    public TypedArrayTest() {
+
+    private static HeadlessApplication APP;
+    private static Resources RES;
+
+    @BeforeClass
+    public static void init() {
+        final HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+        final ApplicationListener client = new Client(1280, 720);
+        APP = new HeadlessApplication(client, config);
+
+        AssetManager ASSETS = new AssetManager();
+        RES = new Resources(ASSETS);
+    }
+
+    @AfterClass
+    public static void release() {
+        RES = null;
+        APP.exit();
+        APP = null;
     }
 
     @Test
     public void testObtain() {
-        final Resources RES = new Resources(ASSETS);
         TypedArray array = TypedArray.obtain(RES, 5);
         assertTrue(array.data != null);
         assertTrue(array.data.length == 5);
@@ -27,7 +51,6 @@ public class TypedArrayTest {
 
     @Test
     public void testRecycle() {
-        final Resources RES = new Resources(ASSETS);
         TypedArray array = TypedArray.obtain(RES, 5);
         array.recycle();
         assertTrue(array.isRecycled());
@@ -35,12 +58,21 @@ public class TypedArrayTest {
 
     @Test
     public void testReset() {
-        final Resources RES = new Resources(ASSETS);
-        TypedArray array = TypedArray.obtain(RES, 5);
+        final TypedArray array = TypedArray.obtain(RES, 5);
         array.recycle();
         assertTrue(array.isRecycled());
-        assertTrue(array.getResources() == null);
-        assertTrue(array.getTheme() == null);
+        assertThrows(IllegalStateException.class, new Assert.ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                Resources res = array.getResources();
+            }
+        });
+        assertThrows(IllegalStateException.class, new Assert.ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                Resources.Theme theme = array.getTheme();
+            }
+        });
         assertTrue(array.data == null);
     }
     
