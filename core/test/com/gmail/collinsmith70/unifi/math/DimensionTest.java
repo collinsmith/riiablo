39c615e040ea70dc015ffb24985a2635f9659d01
Collin Smith <collinsmith70@gmail.com>
2016-05-12 14:27:59 -0700

    Added test cases for unifi.math.Dimension

---
+++ core/test/com/gmail/collinsmith70/unifi/math/DimensionTest.java
@@ -0,0 +1,120 @@
+package com.gmail.collinsmith70.unifi.math;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+public class DimensionTest {
+
+    private static final int[] data = {
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+            43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+    };
+
+    @Test
+    public void testSetWidth() {
+        Dimension dim = new Dimension();
+        for (int testCase : data) {
+            dim.setWidth(testCase);
+            assertTrue(dim.getWidth() == testCase);
+        }
+    }
+
+    @Test
+    public void testSetHeight() {
+        Dimension dim = new Dimension();
+        for (int testCase : data) {
+            dim.setHeight(testCase);
+            assertTrue(dim.getHeight() == testCase);
+        }
+    }
+
+    @Test
+    public void testSet() {
+        Dimension dim = new Dimension();
+        int width, height;
+        for (int i = 0; i < data.length - 1; i++) {
+            width = data[i];
+            height = data[i+1];
+            dim.set(width, height);
+            assertTrue(dim.getWidth() == width);
+            assertTrue(dim.getHeight() == height);
+        }
+    }
+
+    @Test
+    public void testOnChange() {
+        assertThrows(RuntimeException.class, new Assert.ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                new Dimension() {
+                    @Override
+                    protected void onChange() {
+                        throw new RuntimeException("Dimension#onChange() called");
+                    }
+                }.setWidth(1);
+            }
+        });
+        assertThrows(RuntimeException.class, new Assert.ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                new Dimension() {
+                    @Override
+                    protected void onChange() {
+                        throw new RuntimeException("Dimension#onChange() called");
+                    }
+                }.setHeight(1);
+            }
+        });
+        assertThrows(RuntimeException.class, new Assert.ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                new Dimension() {
+                    @Override
+                    protected void onChange() {
+                        throw new RuntimeException("Dimension#onChange() called");
+                    }
+                }.set(1, 1);
+            }
+        });
+    }
+
+    @Test
+    public void testEqualsIntInt() {
+        int width, height;
+        for (int i = 0; i < data.length - 1; i++) {
+            width = data[i];
+            height = data[i + 1];
+            assertTrue(new Dimension(width, height).equals(width, height));
+        }
+    }
+
+    @Test
+    public void testEqualsObject() {
+        Dimension dim1 = new Dimension(0, 0);
+        Dimension dim2 = new Dimension(1, 1);
+        
+        assertTrue(dim1.equals(dim1));
+        assertTrue(dim2.equals(dim2));
+        assertFalse(dim1.equals(dim2));
+        assertFalse(dim2.equals(dim1));
+        assertFalse(dim1.equals(null));
+        assertFalse(dim2.equals(null));
+    }
+
+    @Test
+    public void testHashCode() {
+        Dimension dim1 = new Dimension(0, 0);
+        Dimension dim2 = new Dimension(1, 1);
+        Dimension dim3 = new Dimension(0, 0);
+
+        assertTrue(dim1.hashCode() == dim1.hashCode());
+        assertTrue(dim1.hashCode() == dim3.hashCode());
+        assertFalse(dim1.hashCode() == dim2.hashCode());
+        assertFalse(dim3.hashCode() == dim2.hashCode());
+    }
+
+}
