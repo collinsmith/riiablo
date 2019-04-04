fe9cdd0fd8b3449ce43eb55015d46b7b0ee473f7
Collin Smith <collinsmith70@gmail.com>
2019-04-04 16:17:49 -0700

    Added remaining columns to CharStats excel

--- core/src/com/riiablo/codec/excel/CharStats.java
+++ core/src/com/riiablo/codec/excel/CharStats.java
@@ -15,11 +15,33 @@ public class CharStats extends Excel<CharStats.Entry> {
     @Column(format = "int")
     public int     _int;
     @Column public int     vit;
+    @Column public int     stamina;
+    @Column public int     hpadd;
+    @Column public int     ManaRegen;
+    @Column public int     ToHitFactor;
     @Column public int     WalkVelocity;
     @Column public int     RunVelocity;
+    @Column public int     RunDrain;
+    @Column public int     LifePerLevel;
+    @Column public int     StaminaPerLevel;
+    @Column public int     ManaPerLevel;
+    @Column public int     LifePerVitality;
+    @Column public int     StaminaPerVitality;
+    @Column public int     ManaPerMagic;
+    @Column public int     StatPerLevel;
+    @Column public int     BlockFactor;
     @Column public String  StrAllSkills;
     @Column(startIndex = 1, endIndex = 4)
     public String  StrSkillTab[];
     @Column public String  StrClassOnly;
+    @Column public String  baseWClass;
+    @Column(format = "Skill %d", startIndex = 1, endIndex = 11)
+    public String  Skill[];
+    @Column(format = "item%d", startIndex = 1, endIndex = 11)
+    public String  item[];
+    @Column(format = "item%dloc", startIndex = 1, endIndex = 11)
+    public String  itemloc[];
+    @Column(format = "item%dcount", startIndex = 1, endIndex = 11)
+    public String  itemcount[];
   }
 }
