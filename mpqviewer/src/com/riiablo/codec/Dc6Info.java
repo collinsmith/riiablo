421519eba0d2f56a1ccc9723c3756450d31ac3ee
Collin Smith <collinsmith70@gmail.com>
2019-05-04 16:00:30 -0700

    Added remaining DC6 data infos

--- mpqviewer/src/com/riiablo/codec/Dc6Info.java
+++ mpqviewer/src/com/riiablo/codec/Dc6Info.java
@@ -4,6 +4,7 @@ import com.kotcrab.vis.ui.widget.VisTable;
 
 public class Dc6Info extends VisTable {
   DC6 dc6;
+  VisTable header, box, frame;
 
   public Dc6Info() {}
 
@@ -12,26 +13,59 @@ public class Dc6Info extends VisTable {
     this.dc6 = dc6;
     clear();
 
-    add("version: ",        dc6.header.version);
-    add("flags: ",          dc6.header.flags, true);
-    add("encoding: ",       dc6.header.format);
-    add("pad bytes: ",      dc6.header.termination, true);
-    add("directions: ",     dc6.header.directions);
-    add("frames per dir: ", dc6.header.framesPerDir);
+    header = new VisTable();
+    header.add("Header:").left().colspan(2).row();
+    add(header, "version: ",        dc6.header.version);
+    add(header, "flags: ",          "%08x", dc6.header.flags);
+    add(header, "encoding: ",       dc6.header.format);
+    add(header, "pad bytes: ",      "%08x", dc6.header.termination);
+    add(header, "directions: ",     dc6.header.directions);
+    add(header, "frames per dir: ", dc6.header.framesPerDir);
+
+    box = new VisTable();
+
+    VisTable left = new VisTable();
+    left.add(header).row();
+    left.add().growY().row();
+    left.add(box).row();
+    add(left).growY().spaceRight(8);
+
+    frame = new VisTable();
+    add(frame).growY();
 
     return this;
   }
 
-  private void add(String label, int value) {
-    add(label, value, false);
+  private static VisTable add(VisTable table, String label, int value) {
+    return add(table, label, String.valueOf(value));
   }
 
-  private void add(String label, int value, boolean hex) {
-    add(label).right();
-    add(hex ? String.format("%08x", value) : String.valueOf(value)).left();
-    row();
+  private static VisTable add(VisTable table, String label, String format, Object... args) {
+    table.add(label).right();
+    table.add(String.format(format, args)).left();
+    table.row();
+    return table;
   }
 
   public void update(int d, int f) {
+    DC6.Direction dir = dc6.getDirection(d);
+    DC6.Frame frm = dc6.getFrame(d, f);
+
+    box.clear();
+    box.add("BBox:").left().colspan(2).row();
+    add(box, "width,height: ", "%d,%d", dir.box.width, dir.box.height);
+    add(box, "bbox: ",         "(%d,%d) -> (%d,%d)", dir.box.xMin, dir.box.yMin, dir.box.xMax, dir.box.yMax);
+    add(box, "offset: ",       "(%d,%d)", dir.box.xMin, dir.box.yMax);
+
+    frame.clear();
+    frame.add("Frame:").left().colspan(2).row();
+    add(frame, "direction: ",    d);
+    add(frame, "flipped: ",      frm.flip);
+    add(frame, "width,height: ", "%d,%d", frm.box.width, frm.box.height);
+    add(frame, "offset: ",       "(%d,%d)", frm.box.xMin, frm.box.yMax);
+    add(frame, "alloc size: ",   frm.allocSize);
+    add(frame, "next block: ",   "%08x", frm.nextBlock);
+    add(frame, "len: ",          frm.length);
+    frame.add().growY().row();
   }
 }
