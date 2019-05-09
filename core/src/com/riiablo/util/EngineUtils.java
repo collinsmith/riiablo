a5ff6233c956d7daee89d76189fb403f801dc90c
Collin Smith <collinsmith70@gmail.com>
2019-05-08 23:37:38 -0700

    Removed unneeded qualification

--- core/src/com/riiablo/util/EngineUtils.java
+++ core/src/com/riiablo/util/EngineUtils.java
@@ -2,7 +2,6 @@ package com.riiablo.util;
 
 import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Vector2;
-import com.riiablo.map.DT1;
 import com.riiablo.map.DT1.Tile;
 
 public class EngineUtils {
@@ -10,13 +9,13 @@ public class EngineUtils {
 
   public static Vector2 worldToScreenCoords(Vector2 world, Vector2 dst) {
     assert world != dst;
-    dst.x = +(world.x * Tile.SUBTILE_WIDTH50)  - (world.y * DT1.Tile.SUBTILE_WIDTH50);
+    dst.x = +(world.x * Tile.SUBTILE_WIDTH50)  - (world.y * Tile.SUBTILE_WIDTH50);
     dst.y = -(world.x * Tile.SUBTILE_HEIGHT50) - (world.y * Tile.SUBTILE_HEIGHT50);
     return dst;
   }
 
   public static Vector2 worldToScreenCoords(float x, float y, Vector2 dst) {
-    dst.x = +(x * Tile.SUBTILE_WIDTH50)  - (y * DT1.Tile.SUBTILE_WIDTH50);
+    dst.x = +(x * Tile.SUBTILE_WIDTH50)  - (y * Tile.SUBTILE_WIDTH50);
     dst.y = -(x * Tile.SUBTILE_HEIGHT50) - (y * Tile.SUBTILE_HEIGHT50);
     return dst;
   }
