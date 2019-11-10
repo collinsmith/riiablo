851cdc2aa5ea8e12407eaf9f850ad8600d51cb68
Collin Smith <collinsmith70@gmail.com>
2019-11-09 21:52:11 -0800

    Moved default angle to its own field

--- core/src/com/riiablo/engine/component/AngleComponent.java
+++ core/src/com/riiablo/engine/component/AngleComponent.java
@@ -6,12 +6,13 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Pool;
 
 public class AngleComponent implements Component, Pool.Poolable {
-  public final Vector2 angle  = Vector2.X.cpy().rotateRad(MathUtils.PI / 2);
+  private static final float DEFAULT_ANGLE = MathUtils.PI / 2;
+  public final Vector2 angle  = Vector2.X.cpy().rotateRad(DEFAULT_ANGLE);
   public final Vector2 target = angle.cpy();
 
   @Override
   public void reset() {
-    angle.rotateRad(MathUtils.PI / 2);
+    angle.rotateRad(DEFAULT_ANGLE);
     target.set(angle);
   }
 }
