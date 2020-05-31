94e2b379e7aefda384a4c1fcde453a99169ce0b9
Collin Smith <collinsmith70@gmail.com>
2020-05-30 20:36:39 -0700

    Removed unneeded annotation

--- core/src/com/riiablo/engine/server/ItemManager.java
+++ core/src/com/riiablo/engine/server/ItemManager.java
@@ -1,7 +1,6 @@
 package com.riiablo.engine.server;
 
 import com.artemis.ComponentMapper;
-import com.artemis.annotations.All;
 import com.riiablo.engine.server.component.Item;
 import com.riiablo.engine.server.component.Player;
 import com.riiablo.item.BodyLoc;
@@ -9,7 +8,6 @@ import com.riiablo.item.StoreLoc;
 
 import net.mostlyoriginal.api.system.core.PassiveSystem;
 
-@All(Player.class)
 public class ItemManager extends PassiveSystem {
   private static final String TAG = "ItemManager";
 
