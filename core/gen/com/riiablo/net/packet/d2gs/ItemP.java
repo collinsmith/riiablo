4bef27052c20cfadae4101b16b4575768c9edb7f
Collin Smith <collinsmith70@gmail.com>
2020-06-04 20:16:02 -0700

    Disabled optimize imports on the fly for the specified classes
    Flatbuffers files were being modified by IntelliJ before committing

--- core/gen/com/riiablo/net/packet/d2gs/ItemP.java
+++ core/gen/com/riiablo/net/packet/d2gs/ItemP.java
@@ -2,11 +2,10 @@
 
 package com.riiablo.net.packet.d2gs;
 
-import com.google.flatbuffers.FlatBufferBuilder;
-import com.google.flatbuffers.Table;
-
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+import java.nio.*;
+import java.lang.*;
+import java.util.*;
+import com.google.flatbuffers.*;
 
 @SuppressWarnings("unused")
 public final class ItemP extends Table {
