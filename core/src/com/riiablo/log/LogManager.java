b573bd4fa798bdd2559f0b87b6744fc7164b0fc2
Collin Smith <collinsmith70@gmail.com>
2020-08-15 13:34:11 -0700

    Added programmic loading of log4j2 configuration file
    Committing log4j2.xml with default settings (to be ammended)

--- core/src/com/riiablo/log/LogManager.java
+++ core/src/com/riiablo/log/LogManager.java
@@ -1,12 +1,30 @@
 package com.riiablo.log;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.SortedMap;
 import org.apache.commons.collections4.Trie;
 import org.apache.commons.collections4.trie.PatriciaTrie;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
 
 public enum LogManager {
   INSTANCE;
+
+  // DO NOT USE GDX METHODS HERE
+  // THIS BLOCK SHOULD BE GDX AGNOSTIC
+  static {
+    // -Dlog4j.configurationFile=log4j2.xml
+    System.out.println("Initializing log4j2 configuration file...");
+    try {
+      String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j2.xml";
+      ConfigurationSource source = new ConfigurationSource(new FileInputStream(log4jConfigFile));
+      Configurator.initialize(null, source);
+    } catch (Throwable t) {
+      t.printStackTrace();
+    }
+  }
 
   public static Logger getLogger(Class<?> clazz) {
     return INSTANCE.get(clazz);
