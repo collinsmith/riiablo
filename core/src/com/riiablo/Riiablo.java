28fbed711b34f4ed5804fe438a308eaaa981969e
Collin Smith <collinsmith70@gmail.com>
2020-08-17 19:19:57 -0700

    Renamed Riiablo#MAX_ACTS to Riiablo#NUM_ACTS and Riiablo#MAX_CLASSES to Riiablo#NUM_CLASSES

--- core/src/com/riiablo/Riiablo.java
+++ core/src/com/riiablo/Riiablo.java
@@ -44,8 +44,8 @@ public class Riiablo {
   public static final byte ASSASSIN    = 6;
 
   public static final int NUM_DIFFS = 3;
-  public static final int MAX_ACTS = 5;
-  public static final int MAX_CLASSES = 7;
+  public static final int NUM_ACTS = 5;
+  public static final int NUM_CLASSES = 7;
   public static final int MAX_PLAYERS = 8;
   public static final int MAX_NAME_LENGTH = 15;
 
