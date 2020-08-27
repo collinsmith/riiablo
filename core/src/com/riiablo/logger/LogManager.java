28bb1c2ae57dccccc82db09a4ca8a38c86e53f78
Collin Smith <collinsmith70@gmail.com>
2020-08-27 04:30:40 -0700

    Filled remaining methods and reduced visibility of INSTANCE to private

--- core/src/com/riiablo/logger/LogManager.java
+++ core/src/com/riiablo/logger/LogManager.java
@@ -3,7 +3,7 @@ package com.riiablo.logger;
 import org.apache.commons.lang3.StringUtils;
 
 public class LogManager {
-  public static final LoggerRegistry INSTANCE = new LoggerRegistry();
+  private static final LoggerRegistry INSTANCE = new LoggerRegistry();
 
   public static final String ROOT = StringUtils.EMPTY;
 
@@ -13,6 +13,22 @@ public class LogManager {
 
   public static Logger getLogger(String name) {
     return INSTANCE.getLogger(name);
+  }
+
+  public static Logger getRootLogger() {
+    return INSTANCE.getRoot();
+  }
+
+  public static Level getLevel(String name) {
+    return INSTANCE.getLevel(name);
+  }
+
+  public static void setLevel(String name, Level level) {
+    INSTANCE.setLevel(name, level);
+  }
+
+  public static void setLevel(String name, Level level, boolean force) {
+    INSTANCE.setLevel(name, level, force);
   }
 
   private LogManager() {}
