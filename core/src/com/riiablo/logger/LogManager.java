4db52a5e39a8371cd632a502e63e0064418173b2
Collin Smith <collinsmith70@gmail.com>
2020-08-27 22:57:49 -0700

    Renamed INSTANCE to DEFAULT_REGISTRY

--- core/src/com/riiablo/logger/LogManager.java
+++ core/src/com/riiablo/logger/LogManager.java
@@ -3,36 +3,36 @@ package com.riiablo.logger;
 import org.apache.commons.lang3.StringUtils;
 
 public class LogManager {
-  private static final LoggerRegistry INSTANCE = new LoggerRegistry();
+  private static final LoggerRegistry DEFAULT_REGISTRY = new LoggerRegistry();
 
   public static final String ROOT = StringUtils.EMPTY;
 
   public static LoggerRegistry getRegistry() {
-    return INSTANCE;
+    return DEFAULT_REGISTRY;
   }
 
   public static Logger getLogger(Class<?> clazz) {
-    return INSTANCE.getLogger(clazz);
+    return DEFAULT_REGISTRY.getLogger(clazz);
   }
 
   public static Logger getLogger(String name) {
-    return INSTANCE.getLogger(name);
+    return DEFAULT_REGISTRY.getLogger(name);
   }
 
   public static Logger getRootLogger() {
-    return INSTANCE.getRoot();
+    return DEFAULT_REGISTRY.getRoot();
   }
 
   public static Level getLevel(String name) {
-    return INSTANCE.getLevel(name);
+    return DEFAULT_REGISTRY.getLevel(name);
   }
 
   public static void setLevel(String name, Level level) {
-    INSTANCE.setLevel(name, level);
+    DEFAULT_REGISTRY.setLevel(name, level);
   }
 
   public static void setLevel(String name, Level level, boolean force) {
-    INSTANCE.setLevel(name, level, force);
+    DEFAULT_REGISTRY.setLevel(name, level, force);
   }
 
   private LogManager() {}
