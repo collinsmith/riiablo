8cbcb814d15b1d753468cdebd5d7c9db8e35ade2
Collin Smith <collinsmith70@gmail.com>
2020-09-02 16:27:55 -0700

    inlined NUM_GEMPROPS so Item can remain unchanged for the time being

--- core/src/com/riiablo/attributes/GemAttributes.java
+++ core/src/com/riiablo/attributes/GemAttributes.java
@@ -1,9 +1,7 @@
 package com.riiablo.attributes;
 
-import com.riiablo.item.Item;
-
 public class GemAttributes extends Attributes {
-  private static final int NUM_GEMPROPS = Item.NUM_GEMPROPS; // TODO: move this somewhere else
+  private static final int NUM_GEMPROPS = 3; // TODO: move this somewhere else
 
   GemAttributes() {
     super(StatList.obtain(NUM_GEMPROPS));
