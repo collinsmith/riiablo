0bfa47bd8394c579427af9c3247b14d4b7be9f1f
Collin Smith <collinsmith70@gmail.com>
2020-09-05 18:29:31 -0700

    Changed GemAttributes to be a subclass of AggregatedAttributes
    Gems require item_levelreq stat in their base and are more complex than originally thought

--- core/src/com/riiablo/attributes/GemAttributes.java
+++ core/src/com/riiablo/attributes/GemAttributes.java
@@ -1,12 +1,26 @@
 package com.riiablo.attributes;
 
-public class GemAttributes extends Attributes {
-  GemAttributes() {
-    super(StatList.obtain(StatListFlags.NUM_GEM_LISTS));
+import com.riiablo.logger.LogManager;
+import com.riiablo.logger.Logger;
+
+public class GemAttributes extends AggregateAttributes {
+  private static final Logger log = LogManager.getLogger(GemAttributes.class);
+
+  int selected = -1;
+
+  public int selectedList() {
+    return selected;
   }
 
-  @Override
-  public Attributes reset() {
-    throw new UnsupportedOperationException();
+  public void select(int selected) {
+    if (this.selected >= 0 && selected != this.selected) {
+      log.warn("this.selected(" + this.selected + ") already set");
+    }
+
+    this.selected = selected;
+  }
+
+  public StatListGetter selected() {
+    return list(selected);
   }
 }
