1dabb6f6b89a0f8566d8af5168c4ba166f85a135
Collin Smith <collinsmith70@gmail.com>
2020-09-06 00:20:45 -0700

    Removed content, ultimately not needed

--- core/src/com/riiablo/attributes/GemAttributes.java
+++ core/src/com/riiablo/attributes/GemAttributes.java
@@ -1,32 +1,3 @@
 package com.riiablo.attributes;
 
-import com.riiablo.logger.LogManager;
-import com.riiablo.logger.Logger;
-
-public class GemAttributes extends AggregateAttributes {
-  private static final Logger log = LogManager.getLogger(GemAttributes.class);
-
-  int selected = -1;
-  int flags = StatListFlags.FLAG_NONE;
-
-  public int selectedList() {
-    return selected;
-  }
-
-  public int selectedFlags() {
-    return flags;
-  }
-
-  public void select(int selected) {
-    if (this.selected >= 0 && selected != this.selected) {
-      log.warn("this.selected({}) already set", this.selected);
-    }
-
-    this.selected = selected;
-    this.flags = 1 << selected;
-  }
-
-  public StatListGetter selected() {
-    return list(selected);
-  }
-}
+public class GemAttributes extends AggregateAttributes {}
