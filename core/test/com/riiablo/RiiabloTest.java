7f5149baa7619b8a593917468f811831cef88c7e
Collin Smith <collinsmith70@gmail.com>
2020-09-09 23:55:27 -0700

    Committing test parent class

---
+++ core/test/com/riiablo/RiiabloTest.java
@@ -0,0 +1,27 @@
+package com.riiablo;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+
+import com.riiablo.codec.StringTBLs;
+import com.riiablo.mpq.MPQFileHandleResolver;
+
+public class RiiabloTest {
+  @BeforeClass
+  public static void setup() {
+    Gdx.app = new HeadlessApplication(new ApplicationAdapter() {});
+    Riiablo.home = Gdx.files.absolute("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Diablo II");
+    Riiablo.mpqs = new MPQFileHandleResolver();
+    Riiablo.string = new StringTBLs(Riiablo.mpqs);
+    Riiablo.files = new Files();
+  }
+
+  @AfterClass
+  public static void teardown() {
+    Gdx.app.exit();
+  }
+}
