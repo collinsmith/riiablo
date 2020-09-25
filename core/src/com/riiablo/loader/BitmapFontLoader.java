01d92a69c3a360353d1f447eec61aa0290b0b3a3
Collin Smith <collinsmith70@gmail.com>
2020-09-25 01:03:53 -0700

    Changed capitalization of extensions

--- core/src/com/riiablo/loader/BitmapFontLoader.java
+++ core/src/com/riiablo/loader/BitmapFontLoader.java
@@ -8,9 +8,9 @@ import com.badlogic.gdx.assets.loaders.FileHandleResolver;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Array;
 
-import com.riiablo.graphics.BlendMode;
 import com.riiablo.codec.DC6;
 import com.riiablo.codec.FontTBL;
+import com.riiablo.graphics.BlendMode;
 
 public class BitmapFontLoader extends AsynchronousAssetLoader<FontTBL.BitmapFont, BitmapFontLoader.Params> {
 
@@ -24,8 +24,8 @@ public class BitmapFontLoader extends AsynchronousAssetLoader<FontTBL.BitmapFont
 
   @Override
   public void loadAsync(AssetManager assets, String fileName, FileHandle file, Params params) {
-    dc6 = assets.get(name + ".dc6", DC6.class);
-    FontTBL tbl = FontTBL.loadFromFile(resolve(name + ".tbl"));
+    dc6 = assets.get(name + ".DC6", DC6.class);
+    FontTBL tbl = FontTBL.loadFromFile(resolve(name + ".TBL"));
     data = tbl.data(dc6);
     data.blendMode = params != null ? params.blendMode : BlendMode.LUMINOSITY_TINT;
   }
@@ -42,7 +42,7 @@ public class BitmapFontLoader extends AsynchronousAssetLoader<FontTBL.BitmapFont
   @Override
   public Array<AssetDescriptor> getDependencies(String assets, FileHandle file, Params params) {
     name = file.pathWithoutExtension();
-    return Array.<AssetDescriptor>with(new AssetDescriptor<>(name + ".dc6", DC6.class));
+    return Array.<AssetDescriptor>with(new AssetDescriptor<>(name + ".DC6", DC6.class));
   }
 
   public static class Params extends AssetLoaderParameters<FontTBL.BitmapFont> {
