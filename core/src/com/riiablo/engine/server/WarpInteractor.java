d136e3d59917e7778800f6351af73adf2af37b25
Collin Smith <collinsmith70@gmail.com>
2020-10-09 12:34:32 -0700

    Replaced Pathfinder dependency with Actioneer#moveTo

--- core/src/com/riiablo/engine/server/WarpInteractor.java
+++ core/src/com/riiablo/engine/server/WarpInteractor.java
@@ -2,8 +2,11 @@ package com.riiablo.engine.server;
 
 import com.artemis.ComponentMapper;
 import com.artemis.annotations.Wire;
+import net.mostlyoriginal.api.system.core.PassiveSystem;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
+
 import com.riiablo.codec.excel.LvlWarp;
 import com.riiablo.engine.Engine;
 import com.riiablo.engine.server.component.Box2DBody;
@@ -12,8 +15,6 @@ import com.riiablo.engine.server.component.MapWrapper;
 import com.riiablo.engine.server.component.Position;
 import com.riiablo.engine.server.component.Warp;
 import com.riiablo.map.Map;
-
-import net.mostlyoriginal.api.system.core.PassiveSystem;
 
 public class WarpInteractor extends PassiveSystem implements Interactable.Interactor {
   private static final String TAG = "WarpInteractor";
@@ -24,6 +25,7 @@ public class WarpInteractor extends PassiveSystem implements Interactable.Intera
   protected ComponentMapper<Box2DBody> mBox2DBody;
 
   protected Pathfinder pathfinder;
+  protected Actioneer actioneer;
 
   @Wire(name = "map")
   protected Map map;
@@ -48,6 +50,6 @@ public class WarpInteractor extends PassiveSystem implements Interactable.Intera
     Warp dstWarp = mWarp.get(dstWarpEntity);
     LvlWarp.Entry dstWarpEntry = dstWarp.warp;
     tmpVec2.set(dstWarpPos).add(dstWarpEntry.ExitWalkX, dstWarpEntry.ExitWalkY);
-    pathfinder.findPath(src, tmpVec2, true);
+    actioneer.moveTo(src, tmpVec2);
   }
 }
