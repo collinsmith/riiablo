e660f2bd787b81515d5a1edabe45f69013f7feb4
Collin Smith <collinsmith70@gmail.com>
2020-11-21 03:54:59 -0800

    Created InstallationFinder to search for D2 installations and saved games

---
+++ core/src/main/java/com/riiablo/util/InstallationFinder.java
@@ -0,0 +1,142 @@
+package com.riiablo.util;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.SystemUtils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Array;
+
+import com.riiablo.logger.LogManager;
+import com.riiablo.logger.Logger;
+import com.riiablo.save.D2S;
+
+public abstract class InstallationFinder {
+  private static final Logger log = LogManager.getLogger(InstallationFinder.class);
+
+  private static final String SAVE = "Save";
+  private static final Array EMPTY_ARRAY = new Array(0);
+
+  private InstallationFinder() {}
+
+  static final String[] FOLDERS = {
+      "diablo", "riiablo", "Diablo II"
+  };
+
+  public static boolean isD2Home(FileHandle handle) {
+    if (handle == null) return false;
+    return handle.child("d2data.mpq").exists();
+  }
+
+  public static boolean containsSaves(FileHandle handle) {
+    if (handle == null) return false;
+    final FileHandle[] children = handle.list();
+    for (FileHandle child : children) {
+      if (FilenameUtils.isExtension(child.path(), D2S.EXT)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public static InstallationFinder getInstance() {
+    if (SystemUtils.IS_OS_WINDOWS) {
+      return new WindowsInstallationFinder();
+    } else if (SystemUtils.IS_OS_LINUX) {
+      return new LinuxInstallationFinder();
+    } else if (SystemUtils.IS_OS_MAC) {
+      return new MacInstallationFinder();
+    }
+
+    return null;
+  }
+
+  @SuppressWarnings("unchecked")
+  public Array<FileHandle> getHomeDirs() {
+    return (Array<FileHandle>) EMPTY_ARRAY;
+  }
+
+  @SuppressWarnings("unchecked")
+  public Array<FileHandle> getSaveDirs(FileHandle home) {
+    return (Array<FileHandle>) EMPTY_ARRAY;
+  }
+
+  public static final class WindowsInstallationFinder extends InstallationFinder {
+    private static final String D2_REG_KEY = "Software\\Blizzard Entertainment\\Diablo II";
+
+    private static final String getD2RegKey(String valueName, String defaultValue) {
+      try {
+         return WinRegistry.readString(WinRegistry.HKEY_CURRENT_USER, D2_REG_KEY, valueName);
+      } catch (Throwable t) {
+        log.warn(t.getMessage(), t);
+        return defaultValue;
+      }
+    }
+
+    @Override
+    public Array<FileHandle> getHomeDirs() {
+      final String D2_HOME = SystemUtils.getEnvironmentVariable("D2_HOME", null);
+      final String InstallPath = getD2RegKey("InstallPath", null);
+      final String HOMEDRIVE = SystemUtils.getEnvironmentVariable("HOMEDRIVE", "C:");
+      final String ProgramFiles = SystemUtils.getEnvironmentVariable("ProgramFiles", "C:/Program Files");
+      final String ProgramFiles86 = SystemUtils.getEnvironmentVariable("ProgramFiles(x86)", "C:/Program Files (x86)");
+
+      final Array<String> homeDirs = new Array<>();
+      if (D2_HOME != null) homeDirs.add(D2_HOME);
+      if (InstallPath != null) homeDirs.add(InstallPath);
+      homeDirs.add(SystemUtils.USER_HOME);
+      homeDirs.add(SystemUtils.USER_DIR);
+      homeDirs.add(HOMEDRIVE);
+      homeDirs.add(ProgramFiles);
+      homeDirs.add(ProgramFiles86);
+
+      log.traceEntry("resolve() paths: {}", homeDirs);
+      Array<FileHandle> result = null;
+      for (String path : homeDirs) {
+        FileHandle handle = Gdx.files.absolute(path);
+        log.trace("Trying {}", handle);
+        if (isD2Home(handle)) {
+          if (result == null) result = new Array<>();
+          result.add(handle);
+        } else {
+          for (String folder : FOLDERS) {
+            FileHandle child = handle.child(folder);
+            if (InstallationFinder.isD2Home(child)) {
+              if (result == null) result = new Array<>();
+              result.add(child);
+            }
+          }
+        }
+      }
+
+      return result == null ? super.getHomeDirs() : result;
+    }
+
+    @Override
+    public Array<FileHandle> getSaveDirs(FileHandle home) {
+      final Array<FileHandle> saveDirs = new Array<>();
+      final String D2_SAVE = SystemUtils.getEnvironmentVariable("D2_SAVE", null);
+      log.trace("D2_SAVE: {}", D2_SAVE);
+
+      final String NewSavePath = getD2RegKey("NewSavePath", null);
+      log.trace("NewSavePath: {}", NewSavePath);
+
+      final FileHandle homeSaves = home == null ? null : home.child(SAVE);
+      log.trace("homeSaves: {}", homeSaves);
+
+      if (D2_SAVE != null) saveDirs.add(Gdx.files.absolute(D2_SAVE));
+      if (NewSavePath != null) saveDirs.add(Gdx.files.absolute(NewSavePath));
+      if (home != null) saveDirs.add(homeSaves);
+      return saveDirs;
+    }
+  }
+
+  public static final class LinuxInstallationFinder extends InstallationFinder {
+
+  }
+
+  public static final class MacInstallationFinder extends InstallationFinder {
+
+  }
+}
---
+++ core/src/test/java/com/riiablo/util/InstallationFinderTest.java
@@ -0,0 +1,72 @@
+package com.riiablo.util;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Array;
+
+import com.riiablo.RiiabloTest;
+import com.riiablo.logger.Level;
+import com.riiablo.logger.LogManager;
+
+public class InstallationFinderTest extends RiiabloTest {
+  @BeforeClass
+  public static void before() {
+    LogManager.setLevel(InstallationFinder.class.getName(), Level.TRACE);
+  }
+
+  private InstallationFinder getInstance() {
+    return InstallationFinder.getInstance();
+  }
+
+  @Test
+  public void test_getInstallations() {
+    if (SystemUtils.IS_OS_WINDOWS) {
+      test_getHomeDirs_windows();
+    } else if (SystemUtils.IS_OS_LINUX) {
+      test_getHomeDirs_linux();
+    } else if (SystemUtils.IS_OS_MAC) {
+      test_getHomeDirs_mac();
+    }
+  }
+
+  private void test_getHomeDirs_windows() {
+    final InstallationFinder instance = getInstance();
+    final Array<FileHandle> handles = instance.getHomeDirs();
+    Assert.assertNotNull(handles);
+    System.out.println("found: " + handles);
+  }
+
+  private void test_getHomeDirs_linux() {
+  }
+
+  private void test_getHomeDirs_mac() {
+  }
+
+  @Test
+  public void test_getSaves() {
+    if (SystemUtils.IS_OS_WINDOWS) {
+      test_getSaveDirs_windows();
+    } else if (SystemUtils.IS_OS_LINUX) {
+      test_getSaveDirs_linux();
+    } else if (SystemUtils.IS_OS_MAC) {
+      test_getSaveDirs_mac();
+    }
+  }
+
+  private void test_getSaveDirs_windows() {
+    final InstallationFinder instance = getInstance();
+    final Array<FileHandle> handles = instance.getSaveDirs(null);
+    Assert.assertNotNull(handles);
+    System.out.println("found: " + handles);
+  }
+
+  private void test_getSaveDirs_linux() {
+  }
+
+  private void test_getSaveDirs_mac() {
+  }
+}
