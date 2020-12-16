d2451b08c1c5774a9578b3e853e13a0021f25b49
Collin Smith <collinsmith70@gmail.com>
2020-12-15 23:32:56 -0800

    Changed type param to R from T for record type

--- table/core/src/main/java/com/riiablo/table/Serializer.java
+++ table/core/src/main/java/com/riiablo/table/Serializer.java
@@ -4,11 +4,11 @@ package com.riiablo.table;
  * Defines behaviors necessary to serialize a record to and from a binary
  * format.
  *
- * @param <T> record type
+ * @param <R> record type
  */
-public interface Serializer<T> {
-  void readRecord(T record, DataInput in);
-  void writeRecord(T record, DataOutput out);
-  boolean equals(T e1, T e2);
-  Iterable<Throwable> compare(T e1, T e2);
+public interface Serializer<R> {
+  void readRecord(R record, DataInput in);
+  void writeRecord(R record, DataOutput out);
+  boolean equals(R e1, R e2);
+  Iterable<Throwable> compare(R e1, R e2);
 }
