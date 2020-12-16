8bcca53490ad9ccfcf5245774539b2bd6c735cd7
Collin Smith <collinsmith70@gmail.com>
2020-12-15 17:07:43 -0800

    Added some validation to FieldElement

--- table/annotation-processor/src/main/java/com/riiablo/table/annotation/Constants.java
+++ table/annotation-processor/src/main/java/com/riiablo/table/annotation/Constants.java
@@ -3,10 +3,17 @@ package com.riiablo.table.annotation;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.TypeName;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.Name;
 import org.apache.commons.lang3.ArrayUtils;
 
 final class Constants {
   private Constants() {}
+
+  static final String RESERVED_NAME = "x";
+
+  static boolean isReserved(Name name) {
+    return name.contentEquals(RESERVED_NAME);
+  }
 
   static final ClassName STRING = ClassName.get(String.class);
   static final ClassName PRIMARY_KEY = ClassName.get(PrimaryKey.class);
--- table/annotation-processor/src/main/java/com/riiablo/table/annotation/FieldElement.java
+++ table/annotation-processor/src/main/java/com/riiablo/table/annotation/FieldElement.java
@@ -1,13 +1,28 @@
 package com.riiablo.table.annotation;
 
 import java.util.Collection;
+import java.util.Set;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.VariableElement;
 
 final class FieldElement {
   static FieldElement get(Context context, VariableElement element) {
+    Set<Modifier> modifiers = element.getModifiers();
+    if (!modifiers.contains(Modifier.PUBLIC)) {
+      context.warn(element, "record fields should be declared {}", Modifier.PUBLIC);
+      return null;
+    }
+    if (modifiers.contains(Modifier.FINAL)) {
+      context.error(element, "record fields cannot be {}", Modifier.FINAL);
+      return null;
+    }
+    if (Constants.isReserved(element.getSimpleName())) {
+      context.error(element, "'{}' is an illegal record field name", Constants.RESERVED_NAME);
+      return null;
+    }
+
     FormatElement formatElement = FormatElement.get(context, element);
     PrimaryKeyElement primaryKeyElement = PrimaryKeyElement.get(context, element);
-
     return new FieldElement(element, formatElement, primaryKeyElement);
   }
 
