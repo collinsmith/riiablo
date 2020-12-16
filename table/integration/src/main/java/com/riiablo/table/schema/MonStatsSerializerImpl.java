f9fe8d0c6305e0d0554d34bb3b68ee0e81217308
Collin Smith <collinsmith70@gmail.com>
2020-12-16 01:09:14 -0800

    Made MonStatsSerializerImpl a concrete class

--- table/integration/src/main/java/com/riiablo/table/schema/MonStatsSerializerImpl.java
+++ table/integration/src/main/java/com/riiablo/table/schema/MonStatsSerializerImpl.java
@@ -1,7 +1,28 @@
 package com.riiablo.table.schema;
 
+import com.riiablo.table.DataInput;
+import com.riiablo.table.DataOutput;
 import com.riiablo.table.Serializer;
 
-public abstract class MonStatsSerializerImpl implements Serializer<MonStats> {
+public class MonStatsSerializerImpl implements Serializer<MonStats> {
+  @Override
+  public void readRecord(MonStats record, DataInput in) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void writeRecord(MonStats record, DataOutput out) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean equals(MonStats e1, MonStats e2) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Iterable<Throwable> compare(MonStats e1, MonStats e2) {
+    throw new UnsupportedOperationException();
+  }
 
 }
