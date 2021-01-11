f4705e5775e3bdfe568f4ee1d8d71f27169a382c
Collin Smith <collinsmith70@gmail.com>
2021-01-10 18:34:08 -0800

    Added package-level documentation for excel packages

---
+++ core/src/main/java/com/riiablo/excel/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Second iteration of excel class hierarchy design. Kept for posterity until
+ * third iteration is complete.
+ *
+ * @see com.riiablo.codec.excel.Excel
+ * @see com.riiablo.table
+ */
+package com.riiablo.excel;
---
+++ core/src/main/java/com/riiablo/table/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Third iteration of excel class hierarchy design. Incomplete, not used in
+ * production yet.
+ *
+ * @see com.riiablo.codec.excel.Excel
+ * @see com.riiablo.excel
+ */
+package com.riiablo.table;
