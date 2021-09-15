e342870bd96768608730ef38ce6217e16b131384
Collin Smith <collinsmith70@gmail.com>
2021-09-15 14:01:58 -0700

    Extended capabilities of RiiabloTest
    Added #testAsset(String) to resolve file from test home
    Added #toByteArray(short[]) to simplify ubyte array creation

--- core/src/test/java/com/riiablo/RiiabloTest.java
+++ core/src/test/java/com/riiablo/RiiabloTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.*;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.files.FileHandle;
 
 import com.riiablo.codec.StringTBLs;
 import com.riiablo.logger.LogManager;
@@ -12,6 +13,12 @@ import com.riiablo.logger.OutputStreamAppender;
 import com.riiablo.mpq.MPQFileHandleResolver;
 
 public class RiiabloTest {
+  protected static FileHandle testHome;
+
+  protected static FileHandle testAsset(String filename) {
+    return testHome.child(filename);
+  }
+
   @BeforeAll
   public static void setup() {
     Gdx.app = new HeadlessApplication(new ApplicationAdapter() {});
@@ -21,10 +28,19 @@ public class RiiabloTest {
     Riiablo.files = new Files();
     Riiablo.logs = new GdxLoggerManager(LogManager.getRegistry());
     Riiablo.logs.getRootLogger().addAppender(new OutputStreamAppender(System.out));
+    testHome = Gdx.files.absolute("D:\\mpq");
   }
 
   @AfterAll
   public static void teardown() {
     Gdx.app.exit();
+  }
+
+  public static byte[] toByteArray(final short[] array) {
+    final byte[] bytes = new byte[array.length];
+    for (int i = 0, s = array.length; i < s; i++) {
+      bytes[i] = (byte) array[i];
+    }
+    return bytes;
   }
 }
