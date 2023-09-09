0f226739d1ebc50aa212d02a3cdedbe566e21c8c
Collin Smith <collinsmith70@gmail.com>
2023-09-09 00:20:01 -0700

    Additional functions in AssetUtils for disposing gdx assets quietly

--- core/src/main/java/com/riiablo/asset/AssetUtils.java
+++ core/src/main/java/com/riiablo/asset/AssetUtils.java
@@ -1,11 +1,30 @@
 package com.riiablo.asset;
 
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 
 public final class AssetUtils {
   private AssetUtils() { }
 
   public static void dispose(Object o) {
     if (o instanceof Disposable) ((Disposable) o).dispose();
+  }
+
+  public static void disposeQuietly(Object[] o) {
+    disposeQuietly(o, 0, o.length);
+  }
+
+  public static void disposeQuietly(Object[] o, int off, int len) {
+    for (; off < len; off++) {
+      disposeQuietly(o[off]);
+      o[off] = null;
+    }
+  }
+
+  public static void disposeQuietly(Object o) {
+    try {
+      dispose(o);
+    } catch (GdxRuntimeException ignored) {
+    }
   }
 }
